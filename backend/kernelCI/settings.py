a008e94cb78643c83f025b6398f041be6d6b2c4e
Mariana Sartorato <mariana.sartorato@profusion.mobi>
2024-06-24 15:23:43 -0300

    chore: add django rest framework
    - add django rest framework dependency
    - add an empty app called kernelCI_app

--- backend/kernelCI/settings.py
+++ backend/kernelCI/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'kernelCI_app',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
