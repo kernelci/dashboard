bb450d8fbd06eeabd2bbbe61a30c5cacfa612e1f
lfjnascimento <luiz.nascimento@profusion.mobi>
2024-06-27 13:33:53 -0300

    feat: add backend Dockerfile and entrypoint script

---
+++ backend/utils/docker/entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+export PYTHONOPTIMIZE=${PYTHONOPTIMIZE:-2}
+export PYTHONUNBUFFERED=${PYTHONUNBUFFERED:-1}
+
+source $(poetry env info -C ./backend --path)/bin/activate
+set -e
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+function file_env() {
+  local var="$1"
+  local def="${2:-}"
+  local fileVar="${var}_FILE"
+  local fileVal=`eval echo \$"${fileVar}"`
+  local val=`eval echo \$"${var}"`
+  if [ -n "$val" ] && [ -n "$fileVal" ]; then
+    echo >&2 "error: both $var and $fileVar are set (but are exclusive)"
+    exit 1
+  elif [ -f "$fileVal" ]; then
+    val=`cat $fileVal`
+  elif [ -z "$val" ]; then
+    val="$def"
+  fi
+  export "$var"="$val"
+}
+
+file_env DB_DEFAULT_PASSWORD
+
+export DB_DEFAULT="{
+    \"ENGINE\": \"${DB_DEFAULT_ENGINE:=django.db.backends.postgresql}\",
+    \"NAME\": \"${DB_DEFAULT_NAME:=kernelci}\",
+    \"USER\": \"${DB_DEFAULT_USER:=kernelci}\",
+    \"PASSWORD\": \"$DB_DEFAULT_PASSWORD\",
+    \"HOST\": \"${DB_DEFAULT_HOST:=db}\",
+    \"PORT\": \"${DB_DEFAULT_PORT:=5432}\",
+    \"CONN_MAX_AGE\": ${DB_DEAFULT_CONN_MAX_AGE:=null},
+    \"OPTIONS\": {
+      \"connect_timeout\": ${DB_DEFAULT_TIMEOUT:=2}
+    }
+}"
+
+if [ "$SETUP_DJANGO" = 1 ]; then
+    ./manage.py migrate --noinput
+    exit 0
+fi
+
+exec gunicorn kernelCI.wsgi:application "$@"
