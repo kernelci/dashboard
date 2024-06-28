11cd7fdde6078b9728a85b9382df335f5ca63df7
rfgvieira <rfgvieira@profusion.mobi>
2024-06-28 14:54:49 -0300

    feat: add Top Bar component Closes #18

--- dashboard/src/components/Dashboard/Dashboard.tsx
+++ dashboard/src/components/Dashboard/Dashboard.tsx
@@ -1,12 +1,14 @@
 import SideMenu from "../SideMenu/SideMenu";
 import TreeTable from "../Table/TreeTable";
+import TopBar from "../TopBar/TopBar";
 
 const Dashboard = () : JSX.Element => {
   return (
     <div className="w-full h-full">
       <div className="flex flex-row w-full justify-between">
         <SideMenu />
-        <div className="w-full px-16 pt-64">
+        <TopBar />
+        <div className="w-full px-16 pt-64 bg-lightGray">
           <TreeTable />
         </div>
       </div>
---
+++ dashboard/src/components/TopBar/TopBar.tsx
@@ -0,0 +1,21 @@
+import { FormattedMessage } from "react-intl";
+
+import { Input } from "../ui/input";
+
+const TopBar = (): JSX.Element => {
+  return (
+    <div className="flex fixed top-0 h-20 mx-52 pl-6 pr-12 bg-white w-full">
+      <div className="flex flex-row w-full items-center justify-between">
+        <span className="text-2xl ">
+          <FormattedMessage id="routes.treeMonitor" />
+        </span>
+        <div className="flex w-2/3 px-6 items-center">
+          {/* placeholder for search */}
+          <Input className="w-2/3" type="text" placeholder="Search" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
