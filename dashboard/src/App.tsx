d72236ad70234830407f4df3fbbd12d1c8fbd830
Mariana Sartorato <mariana.sartorato@profusion.mobi>
2024-07-03 16:00:12 -0300

    feat(#20): add react-query
    - create a query client
    - add query client provider to app
    - add use query to get data from tree api

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,11 +1,17 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
 import "./App.css";
 import Dashboard from "./components/Dashboard/Dashboard";
 
+const queryClient = new QueryClient();
+
 function App(): JSX.Element {
   return (
+      <QueryClientProvider client={queryClient}>
       <div className="w-screen h-screen">
         <Dashboard />
       </div>
+      </QueryClientProvider>
   );
 }
 
