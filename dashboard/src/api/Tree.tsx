ee04d5856805a37b7087a92699b5e32f847449a9
lfjnascimento <luiz.nascimento@profusion.mobi>
2024-07-09 17:17:16 -0300

    chore: add axios and create a .env file
    closes #81

--- dashboard/src/api/Tree.tsx
+++ dashboard/src/api/Tree.tsx
@@ -2,13 +2,12 @@ import { useQuery, UseQueryResult } from '@tanstack/react-query';
 
 import type { Tree } from '../types/tree/Tree';
 
+import http from "./api"
+
 const fetchTreeCheckoutData = async (): Promise<Tree[]> => {
-  const res = await fetch('/api/tree');
-  if (!res.ok) {
-    throw new Error('Request to /api/tree response was not ok');
-  }
-  return res.json();
-};
+    const res = await http.get('/api/tree/');
+    return res.data;
+}
 
 export const useTreeTable = (): UseQueryResult => {
   return useQuery({ queryKey: ['treeData'], queryFn: fetchTreeCheckoutData });
---
+++ dashboard/src/api/api.ts
@@ -0,0 +1,7 @@
+import axios from "axios"
+
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
+
+export default axios.create({
+  baseURL: API_BASE_URL,
+});
