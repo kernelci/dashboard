6bbd34d76e0473ad08f879923a4a3c7d859173b1
Mariana Sartorato <mariana.sartorato@profusion.mobi>
2024-07-19 17:18:07 -0300

    chore: add cache for react query

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClient } from '@tanstack/react-query';
 
 import { IntlProvider } from 'react-intl';
 
 import { flatten } from 'flat';
+
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client';
+
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister';
 
 import { messages } from './locales/messages/index';
 import { LOCALES } from './locales/constants';
@@ -15,7 +19,20 @@ import Root from './routes/Root/Root';
 import Trees from './routes/Trees/Trees';
 import TreeDetails from './routes/TreeDetails/TreeDetails';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // eslint-disable-next-line no-magic-numbers
+      gcTime: 1000 * 60 * 30, // 30 minutes,
+      // eslint-disable-next-line no-magic-numbers
+      staleTime: 1000 * 60 * 30, // 30 minutes,
+    },
+  },
+});
+
+const persister = createSyncStoragePersister({
+  storage: window.localStorage,
+});
 
 const router = createBrowserRouter([
   {
@@ -36,10 +53,13 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister }}
+    >
       <IntlProvider messages={flatten(messages[LOCALES.EN_US])} locale="en">
         <RouterProvider router={router} />
       </IntlProvider>
-    </QueryClientProvider>
+    </PersistQueryClientProvider>
   </React.StrictMode>,
 );
