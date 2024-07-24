a8ba99821a1497af164abb7956b3bd2090297786
lfjnascimento <luiz.nascimento@profusion.mobi>
2024-07-24 15:46:07 -0300

    feat: add buildDetails endpoint

--- backend/kernelCI_app/urls.py
+++ backend/kernelCI_app/urls.py
@@ -4,5 +4,6 @@ from kernelCI_app import views
 
 urlpatterns = [
     path('tree/', views.TreeView.as_view(), name='tree'),
-    path('tree/<str:commit_hash>', views.TreeDetails.as_view(), name='treeDetails')
+    path('tree/<str:commit_hash>', views.TreeDetails.as_view(), name='treeDetails'),
+    path('build/<str:build_id>', views.BuildDetails.as_view(), name='buildDetails')
 ]
