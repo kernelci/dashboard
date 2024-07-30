ef4efecb48fc75ede83519c667458050b04b21e6
Wilson Neto <wilson.neto@profusion.mobi>
2024-07-30 19:02:44 -0300

    feat(bootsView): Add Boots view and update urls
    This commit introduces a new view, Boots, in the backend application.
    The Boots view is responsible for handling requests related to boot
    information. It extracts platform and error message from the
    environment and provides a detailed response including error counts,
    config counts, boot history, error count per architecture, compilers
    per architecture, platforms, and error message counts.
    
    The urls.py file is also updated to include a new path for the Boots
    view. This allows the application to route requests to the new view.
    
    A new shell script file, boots-get.sh, is added to test the new
    endpoint.

--- backend/kernelCI_app/urls.py
+++ backend/kernelCI_app/urls.py
@@ -5,5 +5,6 @@ from kernelCI_app import views
 urlpatterns = [
     path('tree/', views.TreeView.as_view(), name='tree'),
     path('tree/<str:commit_hash>', views.TreeDetails.as_view(), name='treeDetails'),
-    path('build/<str:build_id>', views.BuildDetails.as_view(), name='buildDetails')
+    path('build/<str:build_id>', views.BuildDetails.as_view(), name='buildDetails'),
+    path('tree/<str:commit_hash>/boot/', views.Boots.as_view(), name='boots'),
 ]
---
+++ backend/requests/boots-get.sh
@@ -0,0 +1,55 @@
+http 'http://localhost:8000/api/tree/1dd28064d4164a4dc9096fd1a7990d2de15f2bb6/boot/'
+
+
+# {
+#   "errorCounts": {
+#     "MISS": 2,
+#     "PASS": 1,
+#     "ERROR": 1
+#   },
+#   "configCounts": {
+#     "defconfig": 3,
+#     "multi_v7_defconfig": 1
+#   },
+#   "bootHistory": [
+#     {
+#       "start_time": "2024-07-06T00:58:14.830Z",
+#       "status": "MISS"
+#     },
+#     {
+#       "start_time": "2024-07-06T00:48:55.924Z",
+#       "status": "MISS"
+#     },
+#     {
+#       "start_time": "2024-07-06T01:11:10.816Z",
+#       "status": "PASS"
+#     },
+#     {
+#       "start_time": "2024-07-06T00:56:49.278Z",
+#       "status": "ERROR"
+#     }
+#   ],
+#   "errorCountPerArchitecture": {
+#     "arm64": 2,
+#     "arm": 1
+#   },
+#   "compilersPerArchitecture": {
+#     "arm64": [
+#       "gcc-12"
+#     ],
+#     "arm": [
+#       "gcc-12"
+#     ]
+#   },
+#   "platforms": [
+#     "bcm2836-rpi-2-b",
+#     "meson-g12b-a311d-khadas-vim3",
+#     "sc7180-trogdor-lazor-limozeen"
+#   ],
+#   "errorMessageCounts": {
+#     "400 Client Error: Bad Request for url: https://lava.infra.foundries.io/api/v0.2/jobs/?format=json&limit=256": 1,
+#     "No time left for remaining 1 retries. 2 retries out of 3 failed for auto-login-action": 1,
+#     "Invalid TESTCASE signal": 1
+#   }
+# }
+
