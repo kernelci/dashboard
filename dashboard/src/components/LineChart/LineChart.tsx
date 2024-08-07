59f223a8c08e16fef2f2fa93820b952ae84b674a
Wilson Neto <wilson.neto@profusion.mobi>
2024-08-07 09:58:12 -0300

    feat(line-chart): add standalone component
    add component that can be implemented later

---
+++ dashboard/src/components/LineChart/LineChart.tsx
@@ -0,0 +1,51 @@
+import { ComponentProps, ReactElement } from 'react';
+import { LineChart as LineChartComponent } from '@mui/x-charts/LineChart';
+
+import ColoredCircle from '@/components/ColoredCircle/ColoredCircle';
+
+interface ILineChartLabel {
+  text: string;
+  backgroundColor: string;
+}
+
+export const LineChartLabel = ({
+  text,
+  backgroundColor,
+}: ILineChartLabel): JSX.Element => {
+  return (
+    <div className="flex items-center gap-2 pr-6 font-medium text-gray-700">
+      <ColoredCircle
+        className="h-3 w-3"
+        backgroundClassName={backgroundColor}
+      />
+      <span>{text}</span>
+    </div>
+  );
+};
+
+type MUILineChart = typeof LineChartComponent;
+type MUILineChartProps = ComponentProps<MUILineChart>;
+
+type TLineChartProps = {
+  labels: ReactElement;
+  series: MUILineChartProps['series'];
+  xAxis: MUILineChartProps['xAxis'];
+};
+
+export const LineChart = ({
+  labels,
+  series,
+  xAxis,
+}: TLineChartProps): JSX.Element => {
+  return (
+    <div className="px-4">
+      <div className="mb-0 mt-3 flex justify-end gap-2">{labels}</div>
+      <LineChartComponent
+        className="w-full"
+        xAxis={xAxis}
+        series={series}
+        height={300}
+      />
+    </div>
+  );
+};
