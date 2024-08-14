24902e230664e88bca4c7c0c9581e88da559f440
Wilson Neto <wilson.neto@profusion.mobi>
2024-08-14 16:26:38 -0300

    feat(api): add endpoint for test details retrieval
    - Added a new endpoint `/api/tests/<str:test_id>` to retrieve test details.
    - Implemented `TestDetails` view to handle GET requests for test details.
    - Created a new SQL query to fetch test details and their descendants.
    - Added a test script `test-details-get.sh` to verify the new endpoint.
    - Updated `urls.py` to include the new endpoint.
    
    This change allows users to fetch detailed information about specific tests,
    including their build information and related metadata.

--- backend/kernelCI_app/urls.py
+++ backend/kernelCI_app/urls.py
@@ -3,10 +3,13 @@ from kernelCI_app import views
 
 
 urlpatterns = [
-    path('tree/', views.TreeView.as_view(), name='tree'),
-    path('tree/<str:commit_hash>', views.TreeDetails.as_view(), name='treeDetails'),
-    path('build/<str:build_id>', views.BuildDetails.as_view(), name='buildDetails'),
-    path('build/<str:build_id>/tests', views.BuildTests.as_view(), name='buildTests'),
-    path('tree/<str:commit_hash>/tests/', views.TreeTestsView.as_view(), name='treeTests'),
-    path('revision/tests/', views.revisionTests.as_view(), name='revisionTests'),
+    path("tree/", views.TreeView.as_view(), name="tree"),
+    path("revision/tests/", views.revisionTests.as_view(), name="revisionTests"),
+    path("tree/<str:commit_hash>", views.TreeDetails.as_view(), name="treeDetails"),
+    path(
+        "tree/<str:commit_hash>/tests/", views.TreeTestsView.as_view(), name="treeTests"
+    ),
+    path("build/<str:build_id>", views.BuildDetails.as_view(), name="buildDetails"),
+    path("build/<str:build_id>/tests", views.BuildTests.as_view(), name="buildTests"),
+    path("tests/<str:test_id>", views.TestDetails.as_view(), name="testDetails"),
 ]
---
+++ backend/requests/test-details-get.sh
@@ -0,0 +1,27 @@
+# Database tested - `playground_kcidb`
+http 'http://localhost:8000/api/tests/kernelci:staging.kernelci.org:662be1c98baa75de59ce77b0'
+#TODO, find a better test ID which showcase a test with descendants
+
+
+# {
+#   "tests": [
+#     {
+#       "id": "kernelci:staging.kernelci.org:662be1c98baa75de59ce77b0",
+#       "build_id": "kernelci:staging.kernelci.org:662bdd1aa697ce1c37ce7692",
+#       "status": "PASS",
+#       "path": "kselftest-alsa.login",
+#       "log_excerpt": null,
+#       "log_url": null,
+#       "misc": "{\"plan\": \"kselftest-alsa\", \"plan_variant\": \"kselftest-alsa\", \"kernelci_status\": \"PASS\"}",
+#       "environment_misc": "{\"lab\": \"lab-broonie\", \"mach\": \"broadcom\", \"device\": \"bcm2711-rpi-4-b\", \"instance\": \"rpi4-02\", \"rootfs_url\": \"http://storage.kernelci.org/images/rootfs/debian/bookworm-kselftest/20240313.0/arm64/initrd.cpio.gz\"}",
+#       "start_time": "2024-04-26T17:18:01.037Z",
+#       "compiler": "aarch64-linux-gnu-gcc (Debian 10.2.1-6) 10.2.1 20210110",
+#       "architecture": "arm64",
+#       "config_name": "defconfig+arm64-chromebook",
+#       "git_commit_hash": "4ffa3ead5ba2c4ed2ade804492f855f7f06e49d8",
+#       "git_repository_branch": "staging-stable",
+#       "git_repository_url": "https://github.com/kernelci/linux.git"
+#     }
+#   ]
+# }
+#
