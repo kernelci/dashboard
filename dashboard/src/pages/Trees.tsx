31bccf0d3f8553557f3506c9317b9f650b058d23
Mariana Sartorato <mariana.sartorato@profusion.mobi>
2024-08-15 15:27:39 -0300

    fix(#26): page title and content alignment

--- dashboard/src/pages/Trees.tsx
+++ dashboard/src/pages/Trees.tsx
@@ -20,7 +20,7 @@ const Trees = (): JSX.Element => {
 
   return (
     <>
-      <div className="w-full bg-lightGray px-16 pt-24">
+      <div className="w-full bg-lightGray py-10">
         <TreeListingPage inputFilter={inputSearchText} />
       </div>
       <div className="fixed top-0 z-10 mx-52 flex w-full pl-6 pr-12 pt-5">
