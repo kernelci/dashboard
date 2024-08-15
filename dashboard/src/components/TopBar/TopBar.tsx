3f7cc54ae8698e1f1e40849245198dda62648e52
Mariana Sartorato <mariana.sartorato@profusion.mobi>
2024-08-15 15:27:39 -0300

    fix(#26): page title and content alignment

--- dashboard/src/components/TopBar/TopBar.tsx
+++ dashboard/src/components/TopBar/TopBar.tsx
@@ -2,7 +2,7 @@ import { FormattedMessage } from 'react-intl';
 
 const TopBar = (): JSX.Element => {
   return (
-    <div className="fixed top-0 z-10 mx-52 flex h-20 w-full bg-white pl-6 pr-12">
+    <div className="fixed top-0 z-10 mx-52 flex h-20 w-full bg-white px-16">
       <div className="flex w-full flex-row items-center justify-between">
         <span className="text-2xl">
           <FormattedMessage id="routes.treeMonitor" />
