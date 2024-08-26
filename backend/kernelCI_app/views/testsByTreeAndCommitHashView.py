6bbac949c6b8111292772ee8ff9f36dda510e76b
Mariana Sartorato <mariana.sartorato@profusion.mobi>
2024-08-26 13:26:46 -0300

    feat(#148): remove f string from testsByTreeAndCommitHashView

--- backend/kernelCI_app/views/testsByTreeAndCommitHashView.py
+++ backend/kernelCI_app/views/testsByTreeAndCommitHashView.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 from django.views import View
-from kernelCI_app.models import Tests
+from django.db import connection
 
 
 class TestsByTreeAndCommitHash(View):
@@ -11,75 +11,69 @@ class TestsByTreeAndCommitHash(View):
         git_branch_param = request.GET.get("git_branch")
         boot_param_with_wildcard = "boot.%"
         boot_param_exact = "boot"
+
+        query = """
+            SELECT t.id, c.git_repository_url,
+                   c.git_commit_hash, t.id, t.build_id, t.start_time, t.duration,
+                   t.status as status, t.path, b.architecture, b.config_name,
+                   b.compiler, t.environment_misc,
+                   t.environment_comment, t.misc
+            FROM checkouts AS c
+            INNER JOIN builds AS b ON c.id = b.checkout_id
+            INNER JOIN tests AS t ON t.build_id = b.id
+            WHERE c.git_commit_hash = %s
+              AND c.origin = %s
+              AND c.git_repository_url = %s
+              AND c.git_repository_branch = %s
+        """
+
+        # Append the path filter conditionally
         if path_param == 'boot':
-            path_filter = "AND (t.path LIKE %s OR t.path = %s)"
-            params = ([commit_hash, origin_param, git_url_param,
-                       git_branch_param, boot_param_with_wildcard, boot_param_exact])
-        else:
-            path_filter = "AND (t.path NOT LIKE %s AND t.path != %s)"
-            params = ([commit_hash, origin_param, git_url_param,
-                       git_branch_param, boot_param_with_wildcard, boot_param_exact])
-
-        names_map = {
-            "t.id": "id",
-            "c.git_repository_url": "git_repository_url",
-            "c.git_commit_hash": "git_commit_hash",
-            "t.build_id": "build_id",
-            "t.start_time": "start_time",
-            "t.status": "status",
-            "t.path": "path",
-            "b.architecture": "architecture",
-            "b.config_name": "config_name",
-            "b.compiler": "compiler",
-            "t.environment_misc": "environment_misc",
-            "t.environment_comment": "environment_comment",
-            "t.misc": "misc",
-        }
-
-        # TODO: Remove the f string here and use parametrized queries
-        query = Tests.objects.raw(
-            f"""
-                SELECT t.id, c.git_repository_url,
-                c.git_commit_hash, t.id, t.build_id, t.start_time, t.duration,
-                t.status as status, t.path, b.architecture, b.config_name,
-                b.compiler, t.environment_misc,
-                t.environment_comment, t.misc FROM checkouts AS c
-                INNER JOIN builds AS b ON c.id = b.checkout_id
-                INNER JOIN tests AS t ON t.build_id = b.id
-                WHERE c.git_commit_hash = %s AND c.origin = %s AND c.git_repository_url = %s
-                  AND c.git_repository_branch = %s {path_filter}
-                ORDER BY
-                t.path,
-                CASE t.status
-                    WHEN 'FAIL' THEN 1
-                    WHEN 'ERROR' THEN 2
-                    WHEN 'MISS' THEN 3
-                    WHEN 'PASS' THEN 4
-                ELSE 4
-                END;
-            """,
-            params,
-            translations=names_map,
-        )
-
-        tests = []
-        for record in query:
-            print(record)
-            tests.append(
-                {
-                    "id": record.id,
-                    "architecture": record.architecture,
-                    "compiler": record.compiler,
-                    "status": record.status,
-                    "path": record.path,
-                    "duration": record.duration,
-                    "startTime": record.start_time,
-                }
+            query += " AND (t.path LIKE %s OR t.path = %s)"
+            parameters = (
+                commit_hash, origin_param, git_url_param, git_branch_param,
+                boot_param_with_wildcard, boot_param_exact
             )
+        else:
+            query += " AND (t.path NOT LIKE %s AND t.path != %s)"
+            parameters = (
+                commit_hash, origin_param, git_url_param, git_branch_param,
+                boot_param_with_wildcard, boot_param_exact
+            )
+
+        query += """
+            ORDER BY
+              t.path,
+              CASE t.status
+                  WHEN 'FAIL' THEN 1
+                  WHEN 'ERROR' THEN 2
+                  WHEN 'MISS' THEN 3
+                  WHEN 'PASS' THEN 4
+              ELSE 4
+              END;
+        """
+
+        with connection.cursor() as cursor:
+            cursor.execute(
+                query,
+                parameters
+            )
+            rows = cursor.fetchall()
+
+        # Format the results as JSON
+        results = [
+            {
+                "status": row[7],
+                "path": row[8],
+                "duration": row[6],
+                "startTime": row[5],
+            }
+            for row in rows
+        ]
 
         # TODO Validate output
         return JsonResponse(
             {
-                "tests": tests,
+                "tests": results,
             }
         )
