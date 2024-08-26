948d4f14794b37582cc807925c7a1c939278b45a
Mariana Sartorato <mariana.sartorato@profusion.mobi>
2024-08-26 10:14:39 -0300

    feat(#146): add table filter component
    - add a filter by status component
    - make a reusable component

---
+++ dashboard/src/components/Table/TableStatusFilter.tsx
@@ -0,0 +1,37 @@
+import classNames from 'classnames';
+import { useMemo } from 'react';
+
+import { Button } from '@/components/ui/button';
+import { TableFilter } from '@/types/tree/TreeDetails';
+
+interface ITableStatusFilter {
+  onClick: (value: TableFilter) => void;
+  filters: {
+    label: string;
+    value: TableFilter;
+  }[];
+}
+
+const TableStatusFilter = ({ filters, onClick }: ITableStatusFilter): JSX.Element => {
+  const filterButtons = useMemo(
+    () =>
+      filters.map((filter, index) => (
+        <Button
+          variant="outline"
+          key={filter.label}
+          className={classNames(
+            'border border-black',
+            index === 0 ? 'rounded-l-full' : 'rounded-l-none',
+            index === filters.length - 1 ? 'rounded-r-full' : 'rounded-r-none',
+          )}
+          onClick={() => onClick(filter.value)}
+        >
+          {filter.label}
+        </Button>
+      )),
+    [filters],
+  );
+  return <div>{filterButtons}</div>;
+};
+
+export default TableStatusFilter;
