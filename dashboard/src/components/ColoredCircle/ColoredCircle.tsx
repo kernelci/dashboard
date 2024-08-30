18739d36897dbc4c78f0657e5c3f248e1bc378e5
Wilson Neto <wilson.neto@profusion.mobi>
2024-08-30 11:20:40 -0300

    feat: add tooltips to ColoredCircle component
    - Integrated react-intl for internationalization of tooltips.
    - Created TooltipWrapper component to handle tooltip rendering.
    - Updated ColoredCircle to use TooltipWrapper for displaying tooltips.
    - Added tooltip texts to TestStatus and BuildStatus components.
    - Updated messages in locales to include new tooltip texts.

--- dashboard/src/components/ColoredCircle/ColoredCircle.tsx
+++ dashboard/src/components/ColoredCircle/ColoredCircle.tsx
@@ -1,11 +1,38 @@
-import { cn } from '../../lib/utils';
+import { useIntl } from 'react-intl';
+
+import { MessagesKey } from '@/locales/messages';
+
+import { Tooltip, TooltipTrigger, TooltipContent } from '@/components/Tooltip';
+
+import { cn } from '@/lib/utils';
 
 interface IColoredCircle {
-  tooltipText?: string;
+  tooltipText?: MessagesKey;
   quantity?: number;
   className?: string;
   backgroundClassName: string;
 }
+
+const TooltipWrapper = ({
+  tooltipText,
+  children,
+}: {
+  tooltipText?: MessagesKey;
+  children: JSX.Element;
+}): JSX.Element => {
+  const { formatMessage } = useIntl();
+  if (!tooltipText) {
+    return children;
+  }
+  return (
+    <Tooltip>
+      <TooltipTrigger>{children}</TooltipTrigger>
+      {tooltipText && (
+        <TooltipContent>{formatMessage({ id: tooltipText })}</TooltipContent>
+      )}
+    </Tooltip>
+  );
+};
 
 const ColoredCircle = ({
   quantity,
@@ -14,16 +41,17 @@ const ColoredCircle = ({
   tooltipText,
 }: IColoredCircle): JSX.Element => {
   return (
-    <div
-      title={tooltipText}
-      className={cn(
-        'inline-flex h-6 w-fit min-w-6 justify-center rounded-full px-1 text-black',
-        className,
-        backgroundClassName,
-      )}
-    >
-      <span className="text-sm">{quantity}</span>
-    </div>
+    <TooltipWrapper tooltipText={tooltipText}>
+      <div
+        className={cn(
+          'inline-flex h-6 w-fit min-w-6 justify-center rounded-full px-1 text-black',
+          className,
+          backgroundClassName,
+        )}
+      >
+        <span className="text-sm">{quantity}</span>
+      </div>
+    </TooltipWrapper>
   );
 };
 
