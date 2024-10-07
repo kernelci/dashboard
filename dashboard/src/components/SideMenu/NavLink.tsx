7264b25f12cc83804f22354dcdde388f654b9290
francisco <francisco.silva@profusion.mobi>
2024-10-07 12:09:20 -0300

    fix: use Link componente from tanstack insteand of HandleLink Component

--- dashboard/src/components/SideMenu/NavLink.tsx
+++ dashboard/src/components/SideMenu/NavLink.tsx
@@ -1,25 +1,37 @@
 import { FormattedMessage } from 'react-intl';
 
-import { AnchorHTMLAttributes } from 'react';
+import { Link, LinkProps } from '@tanstack/react-router';
 
 import { MessagesKey } from '@/locales/messages';
 
 import { NavigationMenuLink } from '../ui/navigation-menu';
 
-interface INavLink extends AnchorHTMLAttributes<HTMLAnchorElement> {
+/*interface INavLink extends AnchorHTMLAttributes<HTMLAnchorElement> {
   idIntl: MessagesKey;
   icon: JSX.Element;
-}
+  to?: string;
+}*/
 
-const NavLink = ({ icon, idIntl, ...props }: INavLink): JSX.Element => (
-  <NavigationMenuLink asChild>
-    <a className="flex items-center no-underline hover:text-sky-500" {...props}>
-      <span className="mr-3">{icon}</span>
-      <span className="text-center text-sm">
-        <FormattedMessage id={idIntl} />{' '}
-      </span>
-    </a>
-  </NavigationMenuLink>
-);
+type INavLink = LinkProps & {
+  idIntl: MessagesKey;
+  icon: JSX.Element;
+  href?: string;
+};
+
+const NavLink = ({ icon, idIntl, ...props }: INavLink): JSX.Element => {
+  return (
+    <NavigationMenuLink asChild>
+      <Link
+        className="flex items-center no-underline hover:text-sky-500"
+        {...props}
+      >
+        <span className="mr-3">{icon}</span>
+        <span className="text-center text-sm">
+          <FormattedMessage id={idIntl} />{' '}
+        </span>
+      </Link>
+    </NavigationMenuLink>
+  );
+};
 
 export default NavLink;
