a4edf38bf7f5dba7b81049a57dfa14e4f19881e7
francisco <francisco.silva@profusion.mobi>
2024-10-07 14:48:00 -0300

    feature(api): add limit time in tree listing route
    Part of #88

--- backend/requests/tree-listing-fast.sh
+++ backend/requests/tree-listing-fast.sh
@@ -1,3 +1,6 @@
 # If you want to see headers, add -p H to the http call:
 # http -p H 'http://localhost:8000/api/tree/' origin==maestro
 http 'http://localhost:8000/api/tree-fast/' origin==maestro
+
+# If you want to provide another limit to query:
+# http 'http://localhost:8000/api/tree-fast/' origin==maestro intervalInDays==8
--- backend/requests/tree-listing.sh
+++ backend/requests/tree-listing.sh
@@ -1,3 +1,6 @@
 # If you want to see headers, add -p H to the http call:
 # http -p H 'http://localhost:8000/api/tree/' origin==0dayci
 http 'http://localhost:8000/api/tree/' origin==0dayci
+
+# If you want to provide another limit to query:
+# http 'http://localhost:8000/api/tree/' origin==0dayci intervalInDays==4
