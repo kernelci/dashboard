01ad7ea2472618408ff054be0b288b2809d886cd
Marcelo Robert Santos <marcelo.santos@profusion.mobi>
2024-10-10 11:52:43 -0300

    refactor: used usememo

--- dashboard/src/components/Filter/CodeBlock.tsx
+++ dashboard/src/components/Filter/CodeBlock.tsx
@@ -2,6 +2,8 @@ import { LiaInfoCircleSolid } from 'react-icons/lia';
 
 import { FormattedMessage } from 'react-intl';
 
+import { useMemo } from 'react';
+
 import { cn } from '@/lib/utils';
 
 import { Tooltip, TooltipTrigger, TooltipContent } from '@/components/Tooltip';
@@ -13,50 +15,67 @@ type TCodeBlockProps = {
   highlightsClassname?: string;
 };
 
+interface IHighlightedCode {
+  highlightedCode: string;
+  highlightCount: number;
+  failCount: number;
+  errorCount: number;
+}
+
 const CodeBlock = ({
   code,
   highlightsClassname,
 }: TCodeBlockProps): JSX.Element => {
-  let highlightCount = 0;
-  let failCount = 0;
-  let errorCount = 0;
+  const highlightedCode: IHighlightedCode = useMemo(() => {
+    let highlights = 0;
+    let fails = 0;
+    let errors = 0;
 
-  // removes unwanted HTML tags
-  code = code.replace(/(<|>)/gm, match => {
-    match = match.replace(/</, '&lt');
-    match = match.replace(/>/, '&gt');
-    return match;
-  });
+    // matches any < or > and replaces with equivalent &lt or &gt
+    let newCode = code.replace(/[<>]/gm, match => {
+      match = match.replace(/</, '&lt');
+      match = match.replace(/>/, '&gt');
+      return match;
+    });
 
-  // highlights fails and errors
-  code = code.replace(
-    /^.*((\berror(:|\s*-*[0-9]|))|(\bfailed(\sto|\swith|\b|:|\s*[0-9]))|(fail(\b|[0-9]))).*$/gim,
-    match => {
-      highlightCount++;
-      if (match.search(/fail/i) !== -1) {
-        // ignores .failed files and fail flags
-        if (match.search(/(\.+failed|failed(\s*0)+|[|/]fail\b)/i) !== -1) {
-          return '<span class="text-blue">' + match + '</span>';
+    newCode = newCode.replace(
+      // matches any occurrence of error or failed
+      /^.*((\berror(:|\s*-*[0-9]|))|(\bfailed(\sto|\swith|\b|:|\s*[0-9]))|(fail(\b|[0-9]))).*$/gim,
+      match => {
+        highlights++;
+        if (match.search(/fail/i) !== -1) {
+          // matches .failed files, failed 0, or fail flags
+          if (match.search(/(\.+failed|failed(\s*0)+|[|/]fail\b)/i) !== -1) {
+            return '<span class="text-blue">' + match + '</span>';
+          }
+          fails++;
+          return '<span class="text-red">' + match + '</span>';
+        } else {
+          // matches error flags or 0 errors
+          if (match.search(/((0\s*\t*)+error|error"+)/i) !== -1) {
+            return '<span class="text-sky-600">' + match + '</span>';
+          }
+          errors++;
+          return '<span class="text-orange-500">' + match + '</span>';
         }
-        failCount++;
-        return '<span class="text-red">' + match + '</span>';
-      } else {
-        // ignores error flags
-        if (match.search(/((0\s*\t*)+error|error"+)/i) !== -1) {
-          return '<span class="text-sky-600">' + match + '</span>';
-        }
-        errorCount++;
-        return '<span class="text-orange-500">' + match + '</span>';
-      }
-    },
-  );
+      },
+    );
+    return {
+      highlightedCode: newCode,
+      highlightCount: highlights,
+      failCount: fails,
+      errorCount: errors,
+    };
+  }, [code]);
 
   return (
     <div>
       <pre className="max-h-[275px] w-full max-w-[calc(100vw_-_398px)] overflow-x-auto bg-[#DDDDDD] p-4 font-mono text-[#767676]">
-        <div dangerouslySetInnerHTML={{ __html: code }}></div>
+        <div
+          dangerouslySetInnerHTML={{ __html: highlightedCode.highlightedCode }}
+        ></div>
       </pre>
-      {highlightCount > 0 ? (
+      {highlightedCode.highlightCount > 0 && (
         <ul className={cn('flex gap-2 py-4 pl-3', highlightsClassname)}>
           <li>
             <span className="flex items-center gap-1 font-bold">
@@ -81,27 +100,31 @@ const CodeBlock = ({
           </li>
           <li className="flex gap-1">
             <ColoredCircle
-              quantity={failCount}
+              quantity={highlightedCode.failCount}
               backgroundClassName="bg-lightRed"
             />
             <FormattedMessage id="global.fails" defaultMessage={'Fails'} />
           </li>
           <li className="flex gap-1">
             <ColoredCircle
-              quantity={errorCount}
+              quantity={highlightedCode.errorCount}
               backgroundClassName="bg-orange-200"
             />
             <FormattedMessage id="global.errors" defaultMessage={'Errors'} />
           </li>
           <li className="flex gap-1">
             <ColoredCircle
-              quantity={highlightCount - failCount - errorCount}
+              quantity={
+                highlightedCode.highlightCount -
+                highlightedCode.failCount -
+                highlightedCode.errorCount
+              }
               backgroundClassName="bg-mediumGray"
             />
             <FormattedMessage id="global.others" defaultMessage={'Others'} />
           </li>
         </ul>
-      ) : null}
+      )}
     </div>
   );
 };
