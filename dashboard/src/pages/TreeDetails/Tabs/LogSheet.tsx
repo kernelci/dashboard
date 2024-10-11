fa2738bdf8ba34ab5c39402d55f2b3ffaa52f783
Wilson Neto <wilson.neto@profusion.mobi>
2024-10-11 14:13:27 -0300

    feat(log-sheet): integrate to log downloader
    - Integrates the LogSheet with the new log downloader endpoint that
      scrapes website
    - Created LogLink component for reusable log file links.
    - Added LogFilesTable component to display log files in a table.
    - Integrated QuerySwitcher to handle log file data fetching and errors.
    - Updated localization for log sheet title and global logs.
    
    Closes #352

--- dashboard/src/pages/TreeDetails/Tabs/LogSheet.tsx
+++ dashboard/src/pages/TreeDetails/Tabs/LogSheet.tsx
@@ -2,6 +2,8 @@ import { FormattedMessage } from 'react-intl';
 
 import { GrDocumentDownload } from 'react-icons/gr';
 
+import { memo } from 'react';
+
 import BaseCard from '@/components/Cards/BaseCard';
 import ColoredCircle from '@/components/ColoredCircle/ColoredCircle';
 import CodeBlock from '@/components/Filter/CodeBlock';
@@ -14,9 +16,12 @@ import {
 import { DumbTableHeader, TableHead } from '@/components/Table/BaseTable';
 import { Button } from '@/components/ui/button';
 import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
-import { DevOnly } from '@/components/FeatureFlag';
 import { truncateBigText } from '@/lib/string';
+import { useLogFiles } from '@/api/TreeDetails';
+import QuerySwitcher from '@/components/QuerySwitcher/QuerySwitcher';
+import { LogFile } from '@/types/tree/TreeDetails';
 
+//TODO Localize the fallback string
 const FallbackLog = `
  ________________________________
 / Sorry, there is no Log Excerpt \\
@@ -33,22 +38,98 @@ type LogSheetProps = {
   logUrl?: string;
 };
 
+type LogFilesTableProps = {
+  logFiles: LogFile[];
+  logUrl: string;
+};
+
+const LogLink = ({
+  combinedLogUrl,
+  children,
+}: {
+  combinedLogUrl: string;
+  children: string;
+}): JSX.Element => (
+  <a
+    href={combinedLogUrl}
+    className="border-b border-b-transparent transition-all hover:border-b-blue hover:text-blue"
+  >
+    {children}
+  </a>
+);
+
+const LogFilesTable = ({
+  logFiles,
+  logUrl,
+}: LogFilesTableProps): JSX.Element => {
+  return (
+    <Table containerClassName="rounded-none border-none">
+      <DumbTableHeader>
+        <TableHead>
+          <FormattedMessage id="logSheet.fileName" />
+        </TableHead>
+        <TableHead>
+          <FormattedMessage id="logSheet.fileSize" />
+        </TableHead>
+        <TableHead>
+          <FormattedMessage id="global.date" />
+        </TableHead>
+      </DumbTableHeader>
+      <TableBody>
+        {logFiles.map(logFile => {
+          const combinedLogUrl = `${logUrl}/${logFile.specific_log_url}`;
+          return (
+            <TableRow key={logFile.file_name}>
+              <TableCell>
+                <LogLink combinedLogUrl={combinedLogUrl}>
+                  {logFile.file_name}
+                </LogLink>
+              </TableCell>
+              <TableCell>
+                <LogLink combinedLogUrl={combinedLogUrl}>
+                  {logFile.file_size}
+                </LogLink>
+              </TableCell>
+              <TableCell>
+                <LogLink combinedLogUrl={combinedLogUrl}>
+                  {logFile.date}
+                </LogLink>
+              </TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
+  );
+};
+
+const MemoizedLogFilesTable = memo(LogFilesTable);
+
 export const LogSheet = ({
   logExcerpt,
   logUrl,
 }: LogSheetProps): JSX.Element => {
+  const { data: logFilesData, status } = useLogFiles(
+    { logUrl: logUrl ?? '' },
+    { enabled: !!logUrl },
+  );
   return (
     <SheetContent className="flex w-[25rem] flex-col sm:w-full sm:max-w-[44rem]">
       <SheetHeader className="mb-3">
-        <SheetTitle className="text-[1.75rem]">Build Logs</SheetTitle>
+        <SheetTitle className="text-[1.75rem]">
+          <FormattedMessage id="logSheet.title" />
+        </SheetTitle>
       </SheetHeader>
-      <BaseCard className="gap-0" title={<>Logs</>}>
+      <BaseCard className="gap-0" title={<FormattedMessage id="global.logs" />}>
         <div className="px-2 py-3 font-mono text-sm text-[#454545]">
           <FormattedMessage
             id="logSheet.indexOf"
             values={{
               link: (
-                <a href={logUrl} className="flex gap-2">
+                <a
+                  href={logUrl}
+                  className="flex gap-2 transition-all hover:text-blue"
+                >
                   <span>{truncateBigText(logUrl)}</span>
                   <GrDocumentDownload className="text-blue" />
                 </a>
@@ -56,42 +137,27 @@ export const LogSheet = ({
             }}
           />
         </div>
-        {/* TODO Replace with actual data */}
-        <DevOnly>
+        <QuerySwitcher
+          data={logFilesData}
+          status={status}
+          skeletonClassname="h-[3rem]"
+          customError={
+            <div className="p-4 text-center">
+              This log url is not supported in the log viewer yet
+            </div>
+          }
+        >
           <Table containerClassName="rounded-none border-none">
-            <DumbTableHeader>
-              <TableHead>
-                <FormattedMessage id="logSheet.fileName" />
-              </TableHead>
-              <TableHead>
-                <FormattedMessage id="logSheet.fileSize" />
-              </TableHead>
-              <TableHead>
-                <FormattedMessage id="global.date" />
-              </TableHead>
-            </DumbTableHeader>
-            <TableBody>
-              {/* TODO: Replace with actual data */}
-              <TableRow>
-                <TableCell>Placeholder 1</TableCell>
-                <TableCell>Placeholder 2</TableCell>
-                <TableCell>Placeholder 3</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>test.log</TableCell>
-                <TableCell>23mb</TableCell>
-                <TableCell>2024-03-1</TableCell>
-              </TableRow>
-              <TableRow>
-                <TableCell>task.log</TableCell>
-                <TableCell>27mb</TableCell>
-                <TableCell>2024-05-1</TableCell>
-              </TableRow>
-            </TableBody>
+            {logFilesData?.log_files && !!logUrl && (
+              <MemoizedLogFilesTable
+                logFiles={logFilesData?.log_files}
+                logUrl={logUrl}
+              />
+            )}
           </Table>
-        </DevOnly>
+        </QuerySwitcher>
       </BaseCard>
-      <div className="my-8 flex flex-row gap-2">
+      <div className="my-4 flex flex-row gap-2">
         <ColoredCircle quantity={3} backgroundClassName="bg-lightRed" />
         <h2>
           <FormattedMessage
@@ -100,7 +166,6 @@ export const LogSheet = ({
           />
         </h2>
       </div>
-      {/*TODO: Replace with actual log data */}
       <CodeBlock code={logExcerpt ?? FallbackLog} />
       <div className="mt-auto flex justify-end">
         <SheetTrigger>
