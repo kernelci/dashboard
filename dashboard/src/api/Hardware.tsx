c19fb9facd55e57a6bedcdba543c4cc8f7c20d22
Wilson Neto <wilson.neto@profusion.mobi>
2024-11-21 16:21:36 -0300

    feat(hardwareView): considerate head of the tree in the results
    Refactored HardwareView to consolidate __getSlowResult and __getFastResult
    methods into a single _getResults method. Updated the order_by field in
    checkouts_subquery to use "-start_time" for more accurate ordering that
    considerates the head of the branch
    Also fix the logic for calculating builds to account for the array of
    hardwares
    
    Closes #530

--- dashboard/src/api/Hardware.tsx
+++ dashboard/src/api/Hardware.tsx
@@ -3,15 +3,12 @@ import { useQuery } from '@tanstack/react-query';
 
 import { useSearch } from '@tanstack/react-router';
 
-import type {
-  HardwareFastResponse,
-  HardwareListingResponse,
-} from '@/types/hardware';
+import type { HardwareListingResponse } from '@/types/hardware';
 
 import type { TOrigins } from '@/types/general';
 
 import http from './api';
-const fetchTreeCheckoutData = async (
+const fetchHardwareListing = async (
   origin: TOrigins,
   startTimestampInSeconds: number,
   endTimeStampInSeconds: number,
@@ -27,14 +24,9 @@ const fetchTreeCheckoutData = async (
   return res.data;
 };
 
-export const useHardwareListingSlow = (
+export const useHardwareListing = (
   startTimestampInSeconds: number,
   endTimestampInSeconds: number,
-  {
-    enabled,
-  }: {
-    enabled: boolean;
-  },
 ): UseQueryResult<HardwareListingResponse> => {
   const { origin } = useSearch({ from: '/hardware' });
 
@@ -48,47 +40,7 @@ export const useHardwareListingSlow = (
   return useQuery({
     queryKey,
     queryFn: () =>
-      fetchTreeCheckoutData(
-        origin,
-        startTimestampInSeconds,
-        endTimestampInSeconds,
-      ),
-    enabled,
-  });
-};
-
-const fetchHardwareListingFastData = async (
-  origin: TOrigins,
-  startTimestampInSeconds: number,
-  endTimeStampInSeconds: number,
-): Promise<HardwareFastResponse> => {
-  const res = await http.get('/api/hardware/', {
-    params: {
-      startTimestampInSeconds,
-      endTimeStampInSeconds,
-      mode: 'fast',
-      origin,
-    },
-  });
-  return res.data;
-};
-
-export const useHardwareListingFast = (
-  startTimestampInSeconds: number,
-  endTimestampInSeconds: number,
-): UseQueryResult<HardwareFastResponse> => {
-  const { origin } = useSearch({ from: '/hardware' });
-  const queryKey = [
-    'hardwareListingFast',
-    startTimestampInSeconds,
-    endTimestampInSeconds,
-    origin,
-  ];
-
-  return useQuery({
-    queryKey,
-    queryFn: () =>
-      fetchHardwareListingFastData(
+      fetchHardwareListing(
         origin,
         startTimestampInSeconds,
         endTimestampInSeconds,
