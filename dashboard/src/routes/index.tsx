4db563b0fca507faf675db0bc2ce3073b8dbeea9
Murilo Geraldini <murilo.geraldini@profusion.mobi>
2024-12-09 13:36:20 -0300

    feat: update tanstack/react-router to v1.87.1
    Closes #658

--- dashboard/src/routes/index.tsx
+++ dashboard/src/routes/index.tsx
@@ -1,13 +1,18 @@
 import { createFileRoute, redirect } from '@tanstack/react-router';
 import { z } from 'zod';
 
+import { DEFAULT_TIME_SEARCH } from '@/pages/treeConstants';
+import { makeZIntervalInDays } from '@/types/general';
+
 export const HomeSearchSchema = z.object({
   treeSearch: z.string().optional().catch(undefined),
+  intervalInDays: makeZIntervalInDays(DEFAULT_TIME_SEARCH),
 });
 
 export const Route = createFileRoute('/')({
   validateSearch: HomeSearchSchema,
-  loader: ctx => {
-    return redirect({ to: '/tree', search: ctx.location.search });
+  loaderDeps: ({ search }) => ({ search }),
+  loader: ({ deps }) => {
+    throw redirect({ to: '/tree', search: deps.search });
   },
 });
