4add55f208f37b88e074f47cb811a7efdb068bca
francisco <francisco.silva@profusion.mobi>
2024-12-17 09:36:23 -0300

    refactor: commit history chart
    - reuse filter logic in tree/hardware details and commit chart
    - remove builds, tests, boots counts CTE from query
    - change the query to get the earliest commits
    - compute count in python
    
    Part of #640

--- backend/kernelCI_app/helpers/filters.py
+++ backend/kernelCI_app/helpers/filters.py
@@ -1,5 +1,12 @@
-from typing import Optional
-from kernelCI_app.utils import UNKNOWN_STRING
+from typing import Optional, Dict, List
+from django.http import HttpResponseBadRequest
+import re
+from kernelCI_app.utils import (
+    getErrorResponseBody
+)
+
+UNKNOWN_STRING = "Unknown"
+NULL_STRINGS = set(["null", UNKNOWN_STRING, "NULL"])
 
 
 def is_build_invalid(build_valid: Optional[bool]) -> bool:
@@ -48,3 +55,413 @@ def should_increment_test_issue(
     is_issue_from_test = is_known_test_issue or is_unknown_issue
 
     return is_issue_from_test
+
+
+def toIntOrDefault(value, default):
+    try:
+        return int(value)
+    except ValueError:
+        return default
+
+
+class InvalidComparisonOP(
+    Exception,
+):
+    pass
+
+
+class FilterParams:
+    """
+    The param field form has two forms:
+    - with a comparison operator ?filter_<field>_[<comparison_op>]=<value>
+    - wit no comparison operator ?filter_<field>=<value>, the comparison operator `exact` will then be applied
+    - Lists
+        example: filter_category=cat1&filter_category=cat2
+        if a list is received then the comparison operator `in` is applied
+    """
+
+    filter_reg = re.compile(r"^(.*)_\[(.*)\]$")
+    filter_param_prefix = "filter_"
+    comparison_op_type_idx = {"orm": 0, "raw": 1}
+
+    comparison_ops = {
+        "exact": ["exact", "="],
+        "in": ["in", "IN"],
+        "gt": ["gt", ">"],
+        "gte": ["gte", ">="],
+        "lt": ["lt", "<"],
+        "lte": ["lte", "<="],
+        "like": ["like", "LIKE"],
+    }
+
+    string_like_filters = ["boot.path", "test.path"]
+
+    def __init__(self, data: Dict, process_body=False):
+        self.filterTestDurationMin, self.filterTestDurationMax = None, None
+        self.filterBootDurationMin, self.filterBootDurationMax = None, None
+        self.filterBuildDurationMin, self.filterBuildDurationMax = None, None
+        self.filterBootStatus = set()
+        self.filterTestStatus = set()
+        self.filterConfigs = set()
+        self.filterCompiler = set()
+        self.filterArchitecture = set()
+        self.filterHardware = set()
+        self.filterTestPath = ""
+        self.filterBootPath = ""
+        self.filterBuildValid = set()
+        self.filterIssues = {
+            "build": set(),
+            "boot": set(),
+            "test": set()
+        }
+
+        self.filter_handlers = {
+            "boot.status": self._handle_boot_status,
+            "boot.duration": self._handle_boot_duration,
+            "test.status": self._handle_test_status,
+            "test.duration": self._handle_test_duration,
+            "duration": self._handle_build_duration,
+            "config_name": self._handle_config_name,
+            "compiler": self._handle_compiler,
+            "architecture": self._handle_architecture,
+            "valid": self._handle_build_valid,
+            "test.hardware": self._handle_hardware,
+            "test.path": self._handle_path,
+            "boot.path": self._handle_path,
+            "build.issue": self._handle_issues,
+            "boot.issue": self._handle_issues,
+            "test.issue": self._handle_issues,
+        }
+
+        self.filters = []
+        if process_body:
+            self.create_filters_from_body(data)
+        else:
+            self.create_filters_from_req(data)
+
+        self._processFilters()
+
+    def _handle_boot_status(self, current_filter: Dict):
+        self.filterBootStatus.add(current_filter["value"])
+
+    def _handle_boot_duration(self, current_filter: Dict):
+        value = current_filter["value"]
+        operation = current_filter["comparison_op"]
+        if operation == "lte":
+            self.filterBootDurationMax = toIntOrDefault(value, None)
+        else:
+            self.filterBootDurationMin = toIntOrDefault(value, None)
+
+    def _handle_test_status(self, current_filter: Dict):
+        self.filterTestStatus.add(current_filter["value"])
+
+    def _handle_test_duration(self, current_filter: Dict):
+        value = current_filter["value"]
+        operation = current_filter["comparison_op"]
+        if operation == "lte":
+            self.filterTestDurationMax = toIntOrDefault(value, None)
+        else:
+            self.filterTestDurationMin = toIntOrDefault(value, None)
+
+    def _handle_config_name(self, current_filter: Dict):
+        self.filterConfigs.add(current_filter["value"])
+
+    def _handle_compiler(self, current_filter: Dict):
+        self.filterCompiler.add(current_filter["value"])
+
+    def _handle_architecture(self, current_filter: Dict):
+        self.filterArchitecture.add(current_filter["value"])
+
+    def _handle_hardware(self, current_filter: Dict):
+        self.filterHardware.add(current_filter["value"])
+
+    def _handle_path(self, current_filter: Dict):
+        if current_filter["field"] == "boot.path":
+            self.filterBootPath = current_filter["value"]
+        else:
+            self.filterTestPath = current_filter["value"]
+
+    def _handle_build_valid(self, current_filter: Dict):
+        self.filterBuildValid.add(current_filter["value"])
+
+    def _handle_build_duration(self, current_filter: Dict):
+        value = current_filter["value"][0]
+        operation = current_filter["comparison_op"]
+        if operation == "lte":
+            self.filterBuildDurationMax = toIntOrDefault(value, None)
+        else:
+            self.filterBuildDurationMin = toIntOrDefault(value, None)
+
+    def _handle_issues(self, current_filter):
+        tab = current_filter["field"].split('.')[0]
+        self.filterIssues[tab].add(current_filter["value"])
+
+    def _processFilters(self):
+        try:
+            for current_filter in self.filters:
+                field = current_filter["field"]
+                # Delegate to the appropriate handler based on the field
+                if field in self.filter_handlers:
+                    self.filter_handlers[field](current_filter)
+        except InvalidComparisonOP as e:
+            return HttpResponseBadRequest(getErrorResponseBody(str(e)))
+
+    def create_filters_from_body(self, body: Dict):
+        filters = body.get("filter", {})
+        for k in filters.keys():
+            if not k.startswith(self.filter_param_prefix):
+                continue
+            # HACK: Flake8 will always bug with (): so we define a variable here
+            filter_param_prefix_length = len(self.filter_param_prefix)
+            filter_term = k[filter_param_prefix_length:]
+
+            # filter as list
+            filter_data = filters.get(k)
+
+            match = self.filter_reg.match(filter_term)
+
+            if match:
+                field = match.group(1)
+                comparison_op = match.group(2)
+                self.add_filter(field, filter_data[0], comparison_op)
+                continue
+
+            if type(filter_data) is list and len(filter_data) > 0:
+                field = filter_term
+                values = filter_data
+
+                for value in values:
+                    self.add_filter(field, value, "in")
+                continue
+
+            if filter_term in self.string_like_filters:
+                self.add_filter(filter_term, filter_data, "like")
+                continue
+
+            self.add_filter(filter_term, filter_data, "exact")
+
+    def create_filters_from_req(self, request):
+        for k in request.GET.keys():
+            if not k.startswith(self.filter_param_prefix):
+                continue
+            # HACK: Flake8 will always bug with (): so we define a variable here
+            filter_param_prefix_length = len(self.filter_param_prefix)
+            filter_term = k[filter_param_prefix_length:]
+
+            # filter as list
+            if len(request.GET.getlist(k)) > 1:
+                field = filter_term
+                values = request.GET.getlist(k)
+
+                for value in values:
+                    self.add_filter(field, value, "in")
+                continue
+
+            if filter_term in self.string_like_filters:
+                self.add_filter(filter_term, request.GET.get(k), "like")
+                continue
+
+            match = self.filter_reg.match(filter_term)
+            if match:
+                field = match.group(1)
+                comparison_op = match.group(2)
+                self.add_filter(field, request.GET.get(k), comparison_op)
+                continue
+
+            self.add_filter(filter_term, request.GET.get(k), "exact")
+
+    def add_filter(self, field, value, comparison_op):
+        self.validate_comparison_op(comparison_op)
+        self.filters.append(
+            {"field": field, "value": value, "comparison_op": comparison_op}
+        )
+
+    def validate_comparison_op(self, op):
+        if op not in self.comparison_ops.keys():
+            raise InvalidComparisonOP(
+                f"Filter with invalid comparison operator `{op}` found`"
+            )
+
+    def get_comparison_op(self, filter, op_type="orm"):
+        idx = self.comparison_op_type_idx[op_type]
+        return self.comparison_ops[filter["comparison_op"]][idx]
+
+    def get_grouped_filters(self):
+        grouped_filters = {}
+
+        for f in self.filters:
+            field = f["field"]
+            value = f['value']
+            if field not in grouped_filters:
+                grouped_filters[field] = f
+            elif type(grouped_filters[field]['value']) is str:
+                grouped_filters[field]['value'] = [grouped_filters[field]['value'], value]
+            else:
+                grouped_filters[field]['value'].append(value)
+
+        return grouped_filters
+
+    def is_build_filtered_out(
+            self, *,
+            duration: Optional[int],
+            valid: Optional[bool],
+            issue_id: Optional[str]
+    ) -> bool:
+        return (
+            len(self.filterBuildValid) > 0
+            and (str(valid).lower() not in self.filterBuildValid)
+        ) or (
+            (self.filterBuildDurationMax is not None or self.filterBuildDurationMin is not None)
+            and duration is None
+        ) or (
+            self.filterBuildDurationMax is not None and (
+                toIntOrDefault(duration, 0) > self.filterBuildDurationMax
+            )
+        ) or (
+            self.filterBuildDurationMin is not None and (
+                toIntOrDefault(duration, 0) < self.filterBuildDurationMin
+            )
+        ) or (
+            len(self.filterIssues["build"]) > 0
+            and (
+                issue_id not in self.filterIssues["build"]
+                or valid is True
+            )
+        )
+
+    def is_record_filtered_out(
+            self, *,
+            hardwares: Optional[List[str]] = None,
+            architecture: Optional[str],
+            compiler: Optional[str],
+            config_name: Optional[str],
+            hasHardware=True
+    ) -> bool:
+        hardware_compatibles = [UNKNOWN_STRING]
+        record_architecture = UNKNOWN_STRING
+        record_compiler = UNKNOWN_STRING
+        record_config_name = UNKNOWN_STRING
+
+        if hardwares is not None:
+            hardware_compatibles = hardwares
+        if architecture is not None:
+            record_architecture = architecture
+        if compile is not None:
+            record_compiler = compiler
+        if config_name is not None:
+            record_config_name = config_name
+
+        if (
+            (
+                hasHardware
+                and len(self.filterHardware) > 0
+                and (not self.filterHardware.intersection(hardware_compatibles))
+            )
+            or (
+                len(self.filterArchitecture) > 0
+                and (record_architecture not in self.filterArchitecture)
+            )
+            or (
+                len(self.filterCompiler) > 0
+                and (record_compiler not in self.filterCompiler)
+            )
+            or (
+                len(self.filterConfigs) > 0
+                and (record_config_name not in self.filterConfigs)
+            )
+        ):
+            return True
+
+        return False
+
+    def is_boot_filtered_out(
+            self, *,
+            path: Optional[str],
+            status: Optional[str],
+            duration: Optional[int],
+            issue_id: Optional[str] = None,
+            incident_test_id: Optional[str] = "incident_test_id",
+    ) -> bool:
+        if (
+            (
+                self.filterBootPath != ""
+                and (self.filterBootPath not in path)
+            )
+            or (
+                len(self.filterBootStatus) > 0
+                and (status not in self.filterBootStatus)
+            )
+            or (
+                (
+                    self.filterBootDurationMax is not None
+                    or self.filterBootDurationMin is not None
+                )
+                and duration is None
+            )
+            or (
+                self.filterBootDurationMax is not None
+                and (
+                    toIntOrDefault(duration, 0) > self.filterBootDurationMax
+                )
+            )
+            or (
+                self.filterBootDurationMin is not None
+                and (
+                    toIntOrDefault(duration, 0) < self.filterBootDurationMin
+                )
+            )
+            or should_filter_test_issue(
+                self.filterIssues["boot"],
+                issue_id,
+                incident_test_id
+            )
+        ):
+            return True
+
+        return False
+
+    def is_test_filtered_out(
+            self, *,
+            path: Optional[str],
+            status: Optional[str],
+            duration: Optional[int],
+            issue_id: Optional[str] = None,
+            incident_test_id: Optional[str] = "incident_test_id",
+    ) -> bool:
+        if (
+            (
+                self.filterTestPath != ""
+                and (self.filterTestPath not in path)
+            )
+            or (
+                len(self.filterTestStatus) > 0
+                and (status not in self.filterTestStatus)
+            )
+            or (
+                (
+                    self.filterTestDurationMax is not None
+                    or self.filterTestDurationMin is not None
+                )
+                and duration is None
+            )
+            or (
+                self.filterTestDurationMax is not None
+                and (
+                    toIntOrDefault(duration, 0) > self.filterTestDurationMax
+                )
+            )
+            or (
+                self.filterTestDurationMin is not None
+                and (
+                    toIntOrDefault(duration, 0) < self.filterTestDurationMin
+                )
+            )
+            or should_filter_test_issue(
+                self.filterIssues["test"],
+                issue_id,
+                incident_test_id
+            )
+        ):
+            return True
+
+        return False
--- backend/kernelCI_app/views/treeCommitsHistory.py
+++ backend/kernelCI_app/views/treeCommitsHistory.py
@@ -1,13 +1,16 @@
+from kernelCI_app.utils import (
+    getErrorResponseBody
+)
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from django.http import JsonResponse, HttpResponseBadRequest
 from django.db import connection
-from kernelCI_app.utils import (
-    NULL_STRINGS,
+from typing import Dict, List
+from kernelCI_app.helpers.filters import (
+    UNKNOWN_STRING,
     FilterParams,
-    InvalidComparisonOP,
-    getErrorResponseBody,
+    InvalidComparisonOP
 )
 
 
@@ -182,165 +185,67 @@ class TreeCommitsHistory(APIView):
             "git_branch_param": git_branch_param,
         }
 
-        build_counts_where, boot_counts_where, test_counts_where = self.__get_filters(filter_params)
-
-        query = f"""
-        WITH
-        relevant_checkouts AS (
+        query = """
+        WITH earliest_commits AS (
             SELECT
-                id,
+                 id,
                 git_commit_hash,
                 git_commit_name,
                 git_repository_branch,
                 git_repository_url,
                 origin,
-                start_time
-            FROM
-                checkouts
+                start_time,
+                time_order
+            FROM (
+                SELECT
+                    id,
+                    git_commit_hash,
+                    git_commit_name,
+                    git_repository_branch,
+                    git_repository_url,
+                    origin,
+                    start_time,
+                    ROW_NUMBER() OVER (
+                        PARTITION BY git_repository_url, git_repository_branch, origin, git_commit_hash
+                        ORDER BY start_time DESC
+                    ) AS time_order
+                FROM   checkouts
+                WHERE  git_repository_branch = %(git_branch_param)s
+                    AND git_repository_url = %(git_url_param)s
+                    AND origin = %(origin_param)s
+                    AND git_commit_hash IS NOT NULL
+                    AND start_time <= (SELECT Max(start_time) AS head_start_time
+                                        FROM   checkouts
+                                        WHERE  git_commit_hash = %(commit_hash)s
+                                                AND origin = %(origin_param)s
+                                                AND git_repository_url = %(git_url_param)s)
+                ORDER  BY start_time DESC) AS checkouts_time_order
             WHERE
-                git_repository_branch = %(git_branch_param)s
-                AND git_repository_url = %(git_url_param)s
-                AND origin = %(origin_param)s
-                AND git_commit_hash IS NOT NULL
-                AND start_time <= (
-                    SELECT MAX(start_time) as head_start_time
-                    FROM checkouts
-                    WHERE git_commit_hash = %(commit_hash)s
-                )
-            ORDER BY
-                start_time DESC
-        ),
-        earliest_commits AS (
-            SELECT DISTINCT ON (git_commit_hash)
-                git_commit_hash,
-                git_commit_name,
-                MAX(start_time) AS earliest_start_time
-            FROM
-                relevant_checkouts
-            GROUP BY
-                git_commit_hash,
-                git_commit_name
-            ORDER BY
-                git_commit_hash
-        ),
-        build_counts AS (
-            SELECT
-                c.git_commit_hash,
-                SUM(CASE WHEN b.valid = true THEN 1 ELSE 0 END) AS valid_builds,
-                SUM(CASE WHEN b.valid = false THEN 1 ELSE 0 END) AS invalid_builds,
-                SUM(CASE WHEN b.valid IS NULL THEN 1 ELSE 0 END) AS null_builds
-            FROM
-                relevant_checkouts AS c
-            INNER JOIN
-                builds AS b
-                ON c.id = b.checkout_id
-            WHERE
-                {build_counts_where}
-            GROUP BY
-                c.git_commit_hash
-        ),
-        boots_counts AS (
-            SELECT
-                c.git_commit_hash,
-                SUM(CASE WHEN t.status = 'FAIL'
-                    AND (t.path = 'boot' OR t.path LIKE 'boot.%%') THEN 1 ELSE 0 END)
-                    AS boots_fail_count,
-                SUM(CASE WHEN t.status = 'ERROR'
-                    AND (t.path = 'boot' OR t.path LIKE 'boot.%%') THEN 1 ELSE 0 END)
-                    AS boots_error_count,
-                SUM(CASE WHEN t.status = 'MISS'
-                    AND (t.path = 'boot' OR t.path LIKE 'boot.%%') THEN 1 ELSE 0 END)
-                    AS boots_miss_count,
-                SUM(CASE WHEN t.status = 'PASS'
-                    AND (t.path = 'boot' OR t.path LIKE 'boot.%%') THEN 1 ELSE 0 END)
-                    AS boots_pass_count,
-                SUM(CASE WHEN t.status = 'DONE'
-                    AND (t.path = 'boot' OR t.path LIKE 'boot.%%') THEN 1 ELSE 0 END)
-                    AS boots_done_count,
-                SUM(CASE WHEN t.status = 'SKIP'
-                    AND (t.path = 'boot' OR t.path LIKE 'boot.%%') THEN 1 ELSE 0 END)
-                    AS boots_skip_count,
-                SUM(CASE WHEN t.status IS NULL
-                    AND (t.path = 'boot' OR t.path LIKE 'boot.%%') THEN 1 ELSE 0 END)
-                    AS boots_null_count
-            FROM
-                relevant_checkouts AS c
-            INNER JOIN
-                builds AS b
-                ON c.id = b.checkout_id
-            LEFT JOIN
-                tests AS t
-                ON b.id = t.build_id
-            WHERE
-                {boot_counts_where}
-            GROUP BY
-                c.git_commit_hash
-        ),
-        test_counts AS (
-            SELECT
-                c.git_commit_hash,
-                SUM(CASE WHEN t.status = 'FAIL' AND (t.path <> 'boot' AND t.path NOT LIKE 'boot.%%')
-                    THEN 1 ELSE 0 END) AS non_boots_fail_count,
-                SUM(CASE WHEN t.status = 'ERROR' AND (t.path <> 'boot' AND t.path NOT LIKE 'boot.%%')
-                    THEN 1 ELSE 0 END) AS non_boots_error_count,
-                SUM(CASE WHEN t.status = 'MISS' AND (t.path <> 'boot' AND t.path NOT LIKE 'boot.%%')
-                    THEN 1 ELSE 0 END) AS non_boots_miss_count,
-                SUM(CASE WHEN t.status = 'PASS' AND (t.path <> 'boot' AND t.path NOT LIKE 'boot.%%')
-                    THEN 1 ELSE 0 END) AS non_boots_pass_count,
-                SUM(CASE WHEN t.status = 'DONE' AND (t.path <> 'boot' AND t.path NOT LIKE 'boot.%%')
-                    THEN 1 ELSE 0 END) AS non_boots_done_count,
-                SUM(CASE WHEN t.status = 'SKIP' AND (t.path <> 'boot' AND t.path NOT LIKE 'boot.%%')
-                    THEN 1 ELSE 0 END) AS non_boots_skip_count,
-                SUM(CASE WHEN t.status IS NULL AND (t.path <> 'boot' AND t.path NOT LIKE 'boot.%%')
-                    THEN 1 ELSE 0 END) AS non_boots_null_count
-            FROM
-                relevant_checkouts AS c
-            INNER JOIN
-                builds AS b
-                ON c.id = b.checkout_id
-            LEFT JOIN
-                tests AS t
-                ON b.id = t.build_id
-            WHERE
-                {test_counts_where}
-            GROUP BY
-                c.git_commit_hash
+                time_order = 1
+            LIMIT 6
         )
         SELECT
-            ec.git_commit_hash,
-            ec.git_commit_name,
-            ec.earliest_start_time,
-            bc.valid_builds,
-            bc.invalid_builds,
-            bc.null_builds,
-            boc.boots_fail_count,
-            boc.boots_error_count,
-            boc.boots_miss_count,
-            boc.boots_pass_count,
-            boc.boots_done_count,
-            boc.boots_skip_count,
-            boc.boots_null_count,
-            tc.non_boots_fail_count,
-            tc.non_boots_error_count,
-            tc.non_boots_miss_count,
-            tc.non_boots_pass_count,
-            tc.non_boots_done_count,
-            tc.non_boots_skip_count,
-            tc.non_boots_null_count
-        FROM
-            earliest_commits AS ec
-        LEFT JOIN
-            build_counts AS bc
-            ON ec.git_commit_hash = bc.git_commit_hash
-        LEFT JOIN
-            boots_counts AS boc
-            ON ec.git_commit_hash = boc.git_commit_hash
-        LEFT JOIN
-            test_counts AS tc
-            ON ec.git_commit_hash = tc.git_commit_hash
-        ORDER BY
-            ec.earliest_start_time DESC
-        LIMIT 6;
+            c.git_commit_hash,
+            c.git_commit_name,
+            c.start_time,
+            b.duration,
+            b.architecture,
+            b.compiler,
+            b.config_name,
+            b.valid,
+            t.path,
+            t.status,
+            t.duration,
+            t.environment_compatible,
+            b.id AS build_id,
+            t.id AS test_id
+        FROM earliest_commits AS c
+        INNER JOIN builds AS b
+        ON
+            c.id = b.checkout_id
+        LEFT JOIN tests AS t
+        ON
+            t.build_id = b.id
         """
 
         # Execute the query
@@ -351,37 +256,38 @@ class TreeCommitsHistory(APIView):
             )
             rows = cursor.fetchall()
 
+        self._process_rows(rows)
         # Format the results as JSON
-        results = [
-            {
-                "git_commit_hash": row[0],
-                "git_commit_name": row[1],
-                "earliest_start_time": row[2],
+        results = []
+
+        for key, value in self.commit_hashes.items():
+            results.append({
+                "git_commit_hash": key,
+                "git_commit_name": value['commit_name'],
+                "earliest_start_time": value['earliest_start_time'],
                 "builds": {
-                    "valid_builds": row[3] or 0,
-                    "invalid_builds": row[4] or 0,
-                    "null_builds": row[5] or 0,
+                    "valid_builds": value['builds_count']['true'],
+                    "invalid_builds": value['builds_count']['false'],
+                    "null_builds": value['builds_count']['null'],
                 },
                 "boots_tests": {
-                    "fail_count": row[6] or 0,
-                    "error_count": row[7] or 0,
-                    "miss_count": row[8] or 0,
-                    "pass_count": row[9] or 0,
-                    "done_count": row[10] or 0,
-                    "skip_count": row[11] or 0,
-                    "null_count": row[12] or 0,
+                    "fail_count": value['boots_count']['fail'],
+                    "error_count": value['boots_count']['error'],
+                    "miss_count": value['boots_count']['miss'],
+                    "pass_count": value['boots_count']['pass'],
+                    "done_count": value['boots_count']['done'],
+                    "skip_count": value['boots_count']['skip'],
+                    "null_count": value['boots_count']['null'],
                 },
                 "non_boots_tests": {
-                    "fail_count": row[13] or 0,
-                    "error_count": row[14] or 0,
-                    "miss_count": row[15] or 0,
-                    "pass_count": row[16] or 0,
-                    "done_count": row[17] or 0,
-                    "skip_count": row[18] or 0,
-                    "null_count": row[19] or 0,
+                    "fail_count": value['tests_count']['fail'],
+                    "error_count": value['tests_count']['error'],
+                    "miss_count": value['tests_count']['miss'],
+                    "pass_count": value['tests_count']['pass'],
+                    "done_count": value['tests_count']['done'],
+                    "skip_count": value['tests_count']['skip'],
+                    "null_count": value['tests_count']['null'],
                 },
-            }
-            for row in rows
-        ]
+            })
 
         return JsonResponse(results, safe=False)
