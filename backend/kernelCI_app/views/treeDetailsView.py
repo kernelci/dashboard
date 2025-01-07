171e8f1c1567a281e202813ab6be9273afb25281
Wilson Neto <wilson.neto@profusion.mobi>
2025-01-07 12:51:57 -0300

    wip

--- backend/kernelCI_app/views/treeDetailsView.py
+++ backend/kernelCI_app/views/treeDetailsView.py
@@ -5,6 +5,7 @@ from kernelCI_app.helpers.filters import (
     UNKNOWN_STRING,
     FilterParams,
 )
+from kernelCI_app.helpers.treeDetails import get_tree_details_data
 from kernelCI_app.utils import (
     convert_issues_dict_to_list,
     extract_error_message,
@@ -394,97 +395,11 @@ class TreeDetails(View):
 
     def get(self, request, commit_hash: str | None):
         cache_key = "treeDetailsSlow"
-        origin_param = request.GET.get("origin")
-        git_url_param = request.GET.get("git_url")
-        git_branch_param = request.GET.get("git_branch")
+
+        rows = get_tree_details_data(request, commit_hash)
+
         self.filterParams = FilterParams(request)
         self.setup_filters()
-
-        params = {
-            "commit_hash": commit_hash,
-            "origin_param": origin_param,
-            "git_url_param": git_url_param,
-            "git_branch_param": git_branch_param,
-        }
-
-        rows = getQueryCache(cache_key, params)
-
-        if rows is None:
-            query = """
-            SELECT
-                    tests.build_id AS tests_build_id,
-                    tests.id AS tests_id,
-                    tests.origin AS tests_origin,
-                    tests.environment_comment AS tests_environment_comment,
-                    tests.environment_misc AS tests_environment_misc,
-                    tests.path AS tests_path,
-                    tests.comment AS tests_comment,
-                    tests.log_url AS tests_log_url,
-                    tests.status AS tests_status,
-                    tests.waived AS tests_waived,
-                    tests.start_time AS tests_start_time,
-                    tests.duration AS tests_duration,
-                    tests.number_value AS tests_number_value,
-                    tests.misc AS tests_misc,
-                    tests.environment_compatible AS tests_environment_compatible,
-                    builds_filter.*,
-                    incidents.id AS incidents_id,
-                    incidents.test_id AS incidents_test_id,
-                    incidents.present AS incidents_present,
-                    issues.id AS issues_id,
-                    issues.comment AS issues_comment,
-                    issues.report_url AS issues_report_url
-            FROM
-                (
-                    SELECT
-                        builds.checkout_id AS builds_checkout_id,
-                        builds.id AS builds_id,
-                        builds.comment AS builds_comment,
-                        builds.start_time AS builds_start_time,
-                        builds.duration AS builds_duration,
-                        builds.architecture AS builds_architecture,
-                        builds.command AS builds_command,
-                        builds.compiler AS builds_compiler,
-                        builds.config_name AS builds_config_name,
-                        builds.config_url AS builds_config_url,
-                        builds.log_url AS builds_log_url,
-                        builds.valid AS builds_valid,
-                        builds.misc AS builds_misc,
-                        tree_head.*
-                    FROM
-                        (
-                            SELECT
-                                checkouts.id AS checkout_id,
-                                checkouts.git_repository_url AS checkouts_git_repository_url,
-                                checkouts.git_repository_branch AS checkouts_git_repository_branch
-                            FROM
-                                checkouts
-                            WHERE
-                                checkouts.git_commit_hash = %(commit_hash)s AND
-                                checkouts.git_repository_url = %(git_url_param)s AND
-                                checkouts.git_repository_branch = %(git_branch_param)s AND
-                                checkouts.origin = %(origin_param)s
-                        ) AS tree_head
-                    INNER JOIN builds
-                        ON tree_head.checkout_id = builds.checkout_id
-                    WHERE
-                        builds.origin = %(origin_param)s
-                ) AS builds_filter
-            LEFT JOIN tests
-                ON builds_filter.builds_id = tests.build_id
-            LEFT JOIN incidents
-                ON tests.id = incidents.test_id OR
-                   builds_filter.builds_id = incidents.build_id
-            LEFT JOIN issues
-                ON incidents.issue_id = issues.id
-            WHERE
-                tests.origin = %(origin_param)s OR
-                tests.origin IS NULL
-            """
-            with connection.cursor() as cursor:
-                cursor.execute(query, params)
-                rows = cursor.fetchall()
-                setQueryCache(cache_key, params, rows)
 
         self._sanitize_rows(rows)
 
