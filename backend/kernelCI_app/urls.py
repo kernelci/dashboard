acb1fc0f9cdb0318b8d2645a321e20c1d4153ed8
Wilson Neto <wilson.neto@profusion.mobi>
2025-01-13 10:02:50 -0300

    feat: add OpenAPI schema generation
    - Added `generate-schema.sh` script to update OpenAPI schema.
    - Updated `README.md` with instructions for OpenAPI schema generation.
    - Added `drf-spectacular` to `INSTALLED_APPS` in `settings.py`.
    - Configured `REST_FRAMEWORK` and `SPECTACULAR_SETTINGS` in `settings.py`.
    - Updated `poetry.lock` and `pyproject.toml` to include `drf-spectacular`.
    - Modified `package.json` to clear Vite cache before running dev server.
    
    Part of #719

--- backend/kernelCI_app/urls.py
+++ backend/kernelCI_app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from django.views.decorators.cache import cache_page
 from django.conf import settings
 from kernelCI_app import views
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 timeout = settings.CACHE_TIMEOUT
 cache = cache_page(timeout)
@@ -76,4 +77,8 @@ urlpatterns = [
     path("issue/<str:issue_id>/version/<str:version>/builds",
          viewCache(views.IssueDetailsBuilds),
          name="issueDetailsBuilds"),
+    # DRF Spectacular
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
