74b9f8a64ccd4c3a85aaa233941a3c0efccddcae
francisco <francisco.silva@profusion.mobi>
2025-01-23 14:00:34 -0300

    feat(dashboard): add treat error return
    - create type to response with error prop
    - create function to throw a error message
    
    Part of #784

---
+++ dashboard/src/api/commonRequest.ts
@@ -0,0 +1,27 @@
+import type { AxiosRequestConfig } from 'axios';
+
+import type { ResponseData } from '@/types/general';
+
+import http from './api';
+
+export class RequestData {
+  static async get<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    const res = await http.get<ResponseData<T>>(url, config);
+
+    if (res.data.error) throw new Error(res.data.error);
+
+    return res.data;
+  }
+
+  static async post<T>(
+    url: string,
+    data?: unknown,
+    config?: AxiosRequestConfig,
+  ): Promise<T> {
+    const res = await http.post<ResponseData<T>>(url, data, config);
+
+    if (res.data.error) throw new Error(res.data.error);
+
+    return res.data;
+  }
+}
