ffc7e373e312f3e70ae7e85435f7dfcacc761cea
Murilo Geraldini <murilo.geraldini@profusion.mobi>
2025-01-27 14:35:16 -0300

    feat(listing-page): add link to status counts
    - Added a different link component to each status counts that automatic
      filters for the clicked status when opening the details page for both
      hardware listing page and tree listing page
    
    Closes #821

--- dashboard/src/pages/Hardware/HardwareTable.tsx
+++ dashboard/src/pages/Hardware/HardwareTable.tsx
@@ -14,7 +14,7 @@ import {
   useReactTable,
 } from '@tanstack/react-table';
 
-import { memo, useCallback, useMemo, useState } from 'react';
+import { memo, useMemo, useState } from 'react';
 
 import { FormattedMessage } from 'react-intl';
 
@@ -31,7 +31,10 @@ import {
   TableRow,
 } from '@/components/ui/table';
 
-import { BuildStatus, GroupedTestStatus } from '@/components/Status/Status';
+import {
+  BuildStatusWithLink,
+  GroupedTestStatusWithLink,
+} from '@/components/Status/Status';
 import { TableHeader } from '@/components/Table/TableHeader';
 import { PaginationInfo } from '@/components/Table/PaginationInfo';
 
@@ -45,113 +48,241 @@ import { zPossibleTabValidator } from '@/types/tree/TreeDetails';
 
 import type { ListingTableColumnMeta } from '@/types/table';
 
+import type { TFilter } from '@/types/general';
+
 import { InputTime } from './InputTime';
 
 // TODO Extract and reuse the table
-
-const columns: ColumnDef<HardwareTableItem>[] = [
-  {
-    accessorKey: 'hardware_name',
-    header: ({ column }): JSX.Element => (
-      <TableHeader column={column} intlKey="global.name" />
-    ),
-    meta: {
-      tabTarget: 'global.builds',
-    },
-  },
-  {
-    accessorKey: 'build_status_summary',
-    accessorFn: ({ build_status_summary: buildCount }): number =>
-      buildCount ? sumStatus(buildCount) : 0,
-    header: ({ column }): JSX.Element => (
-      <TableHeader
-        column={column}
-        intlKey="globalTable.build"
-        tooltipId="build.statusTooltip"
-      />
-    ),
-    cell: ({ row }): JSX.Element => {
-      return row.original.build_status_summary ? (
-        <BuildStatus
-          valid={row.original.build_status_summary.valid}
-          invalid={row.original.build_status_summary.invalid}
-          unknown={row.original.build_status_summary.null}
-        />
-      ) : (
-        <FormattedMessage id="global.loading" defaultMessage="Loading..." />
-      );
-    },
-    meta: {
-      tabTarget: 'global.builds',
-    },
-  },
-  {
-    accessorKey: 'boot_status_summary',
-    accessorFn: ({ boot_status_summary: bootStatusCount }): number =>
-      bootStatusCount ? sumStatus(bootStatusCount) : 0,
-    header: ({ column }): JSX.Element => (
-      <TableHeader
-        column={column}
-        intlKey="globalTable.bootStatus"
-        tooltipId="boots.statusTooltip"
-      />
-    ),
-    cell: ({ row }): JSX.Element => {
-      return row.original.boot_status_summary ? (
-        <GroupedTestStatus
-          pass={row.original.boot_status_summary.PASS}
-          skip={row.original.boot_status_summary.SKIP}
-          fail={row.original.boot_status_summary.FAIL}
-          miss={row.original.boot_status_summary.MISS}
-          done={row.original.boot_status_summary.DONE}
-          error={row.original.boot_status_summary.ERROR}
-          nullStatus={row.original.boot_status_summary.NULL}
-        />
-      ) : (
-        <FormattedMessage id="global.loading" defaultMessage="Loading..." />
-      );
-    },
-    meta: {
-      tabTarget: 'global.boots',
-    },
-  },
-  {
-    accessorKey: 'test_status_summary',
-    accessorFn: ({ test_status_summary: testStatusCount }): number =>
-      testStatusCount ? sumStatus(testStatusCount) : 0,
-    header: ({ column }): JSX.Element => (
-      <TableHeader
-        column={column}
-        intlKey="globalTable.test"
-        tooltipId="test.statusTooltip"
-      />
-    ),
-    cell: ({ row }): JSX.Element => {
-      return row.original.test_status_summary ? (
-        <GroupedTestStatus
-          pass={row.original.test_status_summary.PASS}
-          skip={row.original.test_status_summary.SKIP}
-          fail={row.original.test_status_summary.FAIL}
-          miss={row.original.test_status_summary.MISS}
-          done={row.original.test_status_summary.DONE}
-          error={row.original.test_status_summary.ERROR}
-          nullStatus={row.original.test_status_summary.NULL}
-        />
-      ) : (
-        <FormattedMessage id="global.loading" defaultMessage="Loading..." />
-      );
-    },
-    meta: {
-      tabTarget: 'global.tests',
-    },
-  },
-];
 
 interface ITreeTable {
   treeTableRows: HardwareTableItem[];
   startTimestampInSeconds: number;
   endTimestampInSeconds: number;
 }
+
+const getLinkProps = (
+  row: Row<HardwareTableItem>,
+  startTimestampInSeconds: number,
+  endTimestampInSeconds: number,
+  tabTarget?: string,
+  newDiffFilter?: TFilter,
+): LinkProps => {
+  return {
+    from: '/hardware',
+    to: '/hardware/$hardwareId',
+    params: { hardwareId: row.original.hardware_name },
+
+    search: previousSearch => ({
+      ...previousSearch,
+      currentPageTab: zPossibleTabValidator.parse(tabTarget),
+      startTimestampInSeconds,
+      endTimestampInSeconds,
+      diffFilter: { ...previousSearch.diffFilter, ...newDiffFilter },
+    }),
+  };
+};
+
+const getColumns = (
+  startTimestampInSeconds: number,
+  endTimestampInSeconds: number,
+): ColumnDef<HardwareTableItem>[] => {
+  return [
+    {
+      accessorKey: 'hardware_name',
+      header: ({ column }): JSX.Element => (
+        <TableHeader column={column} intlKey="global.name" />
+      ),
+      meta: {
+        tabTarget: 'global.builds',
+      },
+    },
+    {
+      accessorKey: 'build_status_summary',
+      accessorFn: ({ build_status_summary: buildCount }): number =>
+        buildCount ? sumStatus(buildCount) : 0,
+      header: ({ column }): JSX.Element => (
+        <TableHeader
+          column={column}
+          intlKey="globalTable.build"
+          tooltipId="build.statusTooltip"
+        />
+      ),
+      cell: ({ column, row }): JSX.Element => {
+        const tabTarget = (column.columnDef.meta as ListingTableColumnMeta)
+          .tabTarget;
+        return row.original.build_status_summary ? (
+          <BuildStatusWithLink
+            valid={row.original.build_status_summary.valid}
+            invalid={row.original.build_status_summary.invalid}
+            unknown={row.original.build_status_summary.null}
+            validLinkProps={getLinkProps(
+              row,
+              startTimestampInSeconds,
+              endTimestampInSeconds,
+              tabTarget,
+              {
+                buildStatus: { Success: true },
+              },
+            )}
+            invalidLinkProps={getLinkProps(
+              row,
+              startTimestampInSeconds,
+              endTimestampInSeconds,
+              tabTarget,
+              {
+                buildStatus: { Failed: true },
+              },
+            )}
+            unknownLinkProps={getLinkProps(
+              row,
+              startTimestampInSeconds,
+              endTimestampInSeconds,
+              tabTarget,
+              {
+                buildStatus: { Inconclusive: true },
+              },
+            )}
+          />
+        ) : (
+          <FormattedMessage id="global.loading" defaultMessage="Loading..." />
+        );
+      },
+      meta: {
+        tabTarget: 'global.builds',
+      },
+    },
+    {
+      accessorKey: 'boot_status_summary',
+      accessorFn: ({ boot_status_summary: bootStatusCount }): number =>
+        bootStatusCount ? sumStatus(bootStatusCount) : 0,
+      header: ({ column }): JSX.Element => (
+        <TableHeader
+          column={column}
+          intlKey="globalTable.bootStatus"
+          tooltipId="boots.statusTooltip"
+        />
+      ),
+      cell: ({ column, row }): JSX.Element => {
+        const tabTarget = (column.columnDef.meta as ListingTableColumnMeta)
+          .tabTarget;
+        return row.original.boot_status_summary ? (
+          <GroupedTestStatusWithLink
+            pass={row.original.boot_status_summary.PASS}
+            skip={row.original.boot_status_summary.SKIP}
+            fail={row.original.boot_status_summary.FAIL}
+            miss={row.original.boot_status_summary.MISS}
+            done={row.original.boot_status_summary.DONE}
+            error={row.original.boot_status_summary.ERROR}
+            nullStatus={row.original.boot_status_summary.NULL}
+            passLinkProps={getLinkProps(
+              row,
+              startTimestampInSeconds,
+              endTimestampInSeconds,
+              tabTarget,
+              {
+                bootStatus: { PASS: true },
+              },
+            )}
+            failLinkProps={getLinkProps(
+              row,
+              startTimestampInSeconds,
+              endTimestampInSeconds,
+              tabTarget,
+              {
+                bootStatus: { FAIL: true },
+              },
+            )}
+            inconclusiveLinkProps={getLinkProps(
+              row,
+              startTimestampInSeconds,
+              endTimestampInSeconds,
+              tabTarget,
+              {
+                bootStatus: {
+                  MISS: true,
+                  ERROR: true,
+                  SKIP: true,
+                  DONE: true,
+                  NULL: true,
+                },
+              },
+            )}
+          />
+        ) : (
+          <FormattedMessage id="global.loading" defaultMessage="Loading..." />
+        );
+      },
+      meta: {
+        tabTarget: 'global.boots',
+      },
+    },
+    {
+      accessorKey: 'test_status_summary',
+      accessorFn: ({ test_status_summary: testStatusCount }): number =>
+        testStatusCount ? sumStatus(testStatusCount) : 0,
+      header: ({ column }): JSX.Element => (
+        <TableHeader
+          column={column}
+          intlKey="globalTable.test"
+          tooltipId="test.statusTooltip"
+        />
+      ),
+      cell: ({ column, row }): JSX.Element => {
+        const tabTarget = (column.columnDef.meta as ListingTableColumnMeta)
+          .tabTarget;
+        return row.original.test_status_summary ? (
+          <GroupedTestStatusWithLink
+            pass={row.original.test_status_summary.PASS}
+            skip={row.original.test_status_summary.SKIP}
+            fail={row.original.test_status_summary.FAIL}
+            miss={row.original.test_status_summary.MISS}
+            done={row.original.test_status_summary.DONE}
+            error={row.original.test_status_summary.ERROR}
+            nullStatus={row.original.test_status_summary.NULL}
+            passLinkProps={getLinkProps(
+              row,
+              startTimestampInSeconds,
+              endTimestampInSeconds,
+              tabTarget,
+              {
+                testStatus: { PASS: true },
+              },
+            )}
+            failLinkProps={getLinkProps(
+              row,
+              startTimestampInSeconds,
+              endTimestampInSeconds,
+              tabTarget,
+              {
+                testStatus: { FAIL: true },
+              },
+            )}
+            inconclusiveLinkProps={getLinkProps(
+              row,
+              startTimestampInSeconds,
+              endTimestampInSeconds,
+              tabTarget,
+              {
+                testStatus: {
+                  MISS: true,
+                  ERROR: true,
+                  SKIP: true,
+                  DONE: true,
+                  NULL: true,
+                },
+              },
+            )}
+          />
+        ) : (
+          <FormattedMessage id="global.loading" defaultMessage="Loading..." />
+        );
+      },
+      meta: {
+        tabTarget: 'global.tests',
+      },
+    },
+  ];
+};
 
 export function HardwareTable({
   treeTableRows,
@@ -163,27 +294,14 @@ export function HardwareTable({
   const { pagination, paginationUpdater } =
     usePaginationState('hardwareListing');
 
-  const getLinkProps = useCallback(
-    (row: Row<HardwareTableItem>, tabTarget?: string): LinkProps => {
-      return {
-        from: '/hardware',
-        to: '/hardware/$hardwareId',
-        params: { hardwareId: row.original.hardware_name },
-
-        search: previousSearch => ({
-          ...previousSearch,
-          currentPageTab: zPossibleTabValidator.parse(tabTarget),
-          startTimestampInSeconds,
-          endTimestampInSeconds,
-        }),
-      };
-    },
-    [endTimestampInSeconds, startTimestampInSeconds],
-  );
-
   const data = useMemo(() => {
     return treeTableRows;
   }, [treeTableRows]);
+
+  const columns = useMemo(
+    () => getColumns(startTimestampInSeconds, endTimestampInSeconds),
+    [startTimestampInSeconds, endTimestampInSeconds],
+  );
 
   const table = useReactTable({
     data,
@@ -234,7 +352,12 @@ export function HardwareTable({
               <TableCellWithLink
                 key={cell.id}
                 linkClassName="w-full inline-block h-full"
-                linkProps={getLinkProps(row, tabTarget)}
+                linkProps={getLinkProps(
+                  row,
+                  startTimestampInSeconds,
+                  endTimestampInSeconds,
+                  tabTarget,
+                )}
               >
                 {flexRender(cell.column.columnDef.cell, cell.getContext())}
               </TableCellWithLink>
@@ -249,7 +372,12 @@ export function HardwareTable({
         </TableCell>
       </TableRow>
     );
-  }, [getLinkProps, modelRows]);
+  }, [
+    modelRows,
+    columns.length,
+    startTimestampInSeconds,
+    endTimestampInSeconds,
+  ]);
 
   const MemoizedInputTime = memo(InputTime);
 
