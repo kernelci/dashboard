a641e1a659aaea5ea64ba30142d3e097df63195f
Murilo Geraldini <murilo.geraldini@profusion.mobi>
2025-01-29 13:53:41 -0300

    feat(api): add build tests view documentation
    Part of #86
    Closes #815

--- backend/kernelCI_app/views/buildTestsView.py
+++ backend/kernelCI_app/views/buildTestsView.py
@@ -1,20 +1,31 @@
-from django.http import JsonResponse
-from django.views import View
 from http import HTTPStatus
-from kernelCI_app.helpers.errorHandling import create_error_response
+from kernelCI_app.typeModels.buildDetails import BuildTestsResponse
 from kernelCI_app.models import Tests
+from drf_spectacular.utils import extend_schema
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from pydantic import ValidationError
 
 
-class BuildTests(View):
-    def get(self, request, build_id):
+class BuildTests(APIView):
+    @extend_schema(responses=BuildTestsResponse)
+    def get(self, request, build_id: str) -> Response:
         result = Tests.objects.filter(build_id=build_id).values(
             "id", "duration", "status", "path", "start_time", "environment_compatible"
         )
 
         if not result:
-            return create_error_response(
-                error_message="No tests found for this build",
-                status_code=HTTPStatus.OK,
+            return Response(
+                data={"error": "No tests found for this build"},
+                status=HTTPStatus.OK,
             )
 
-        return JsonResponse(list(result), safe=False)
+        try:
+            valid_response = BuildTestsResponse(result)
+        except ValidationError as e:
+            return Response(
+                data=e.json(),
+                status=HTTPStatus.INTERNAL_SERVER_ERROR,
+            )
+
+        return Response(valid_response.model_dump())
