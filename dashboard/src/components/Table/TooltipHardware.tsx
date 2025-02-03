a3d520bd409e18a360cce4c2e90fdd127dc580ea
Murilo Geraldini <murilo.geraldini@profusion.mobi>
2025-02-03 17:23:37 -0300

    feat: change hardware priority in tests table
    Show platform first and enviroment_compatible in the hover if it exists.
    Show only environment_compatible if platform is undefined
    
    Part of #864

--- dashboard/src/components/Table/TooltipHardware.tsx
+++ dashboard/src/components/Table/TooltipHardware.tsx
@@ -4,27 +4,33 @@ import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/Tooltip';
 
 import { sanitizeTableValue } from '@/components/Table/tableUtils';
 
+const tooltipStartIdx = 1;
+
 const TooltipHardware = ({
   hardwares,
 }: {
   hardwares: string[] | undefined;
 }): JSX.Element => {
+  const shouldHaveTooltip = hardwares && hardwares.length > 1;
+
   const hardwaresTooltip = useMemo(() => {
-    return hardwares
-      ? hardwares.map(hardware => (
+    return shouldHaveTooltip
+      ? hardwares.slice(tooltipStartIdx).map(hardware => (
           <div key={hardware} className="text-center">
             <span>{hardware}</span>
             <br />
           </div>
         ))
       : '-';
-  }, [hardwares]);
+  }, [hardwares, shouldHaveTooltip]);
+
+  const hardwareTrigger = sanitizeTableValue(hardwares?.[0], false);
+
+  if (!shouldHaveTooltip) return <span>{hardwareTrigger}</span>;
 
   return (
     <Tooltip>
-      <TooltipTrigger>
-        {sanitizeTableValue(hardwares?.[0], false)}
-      </TooltipTrigger>
+      <TooltipTrigger>{hardwareTrigger}</TooltipTrigger>
       <TooltipContent>{hardwaresTooltip}</TooltipContent>
     </Tooltip>
   );
