f789835b8791851db75f075471e0b8d3cc1d7869
francisco <francisco.silva@profusion.mobi>
2025-02-04 15:23:03 -0300

    feat: improve pagination options
    - change usePaginationState hook to receive a defaultValue of pageSize
    - change PaginationInfo component to receive a paginationChange action
    
    Part of #860

--- dashboard/src/hooks/usePaginationState.tsx
+++ dashboard/src/hooks/usePaginationState.tsx
@@ -4,18 +4,39 @@ import type { Updater, PaginationState } from '@tanstack/react-table';
 
 import type { TableKeys } from '@/utils/constants/tables';
 
+import { ItemsPerPageValues } from '@/utils/constants/general';
+
 import { useFeatureFlag } from './useFeatureFlag';
 
 const DEFAULT_PAGINATION_STATE = { pageIndex: 0, pageSize: 10 } as const;
 
+const DEFAULT_LISTING_SIZE = 10;
+
+const findClosestGreaterNumber = (sortedList: number[], x: number): number => {
+  for (let i = 0; i < sortedList.length; i++) {
+    if (sortedList[i] >= x) return sortedList[i];
+  }
+
+  return sortedList.slice(-1)[0] ?? DEFAULT_LISTING_SIZE;
+};
+
 export const usePaginationState = (
   selectedTable: TableKeys,
+  defaultValue?: number,
 ): {
   pagination: PaginationState;
   paginationUpdater: (updater: Updater<PaginationState>) => void;
 } => {
   const { showDev } = useFeatureFlag();
   const [pagination, setPagination] = useState<PaginationState>(() => {
+    if (defaultValue) {
+      const pageSize = findClosestGreaterNumber(
+        ItemsPerPageValues,
+        defaultValue,
+      );
+      return { pageIndex: DEFAULT_PAGINATION_STATE.pageIndex, pageSize };
+    }
+
     const storageData = window.localStorage.getItem(selectedTable);
     if (storageData && showDev) {
       try {
