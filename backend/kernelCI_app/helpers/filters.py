d030ae498ac0f65282b97e64de69e4ecba27cd8c
Murilo Geraldini <murilo.geraldini@profusion.mobi>
2025-02-21 15:46:04 -0300

    feat(tests): add support for filters to clients

--- backend/kernelCI_app/helpers/filters.py
+++ backend/kernelCI_app/helpers/filters.py
@@ -1,3 +1,4 @@
+from collections.abc import Callable
 from typing import Optional, Dict, List, Tuple, TypedDict, Literal, Any, Union
 from django.http import HttpResponseBadRequest
 import re
@@ -249,6 +250,28 @@ def to_int_or_default(value, default):
         return default
 
 
+type FilterFields = Literal[
+    "boot.status",
+    "boot.duration",
+    "test.status",
+    "test.duration",
+    "duration",
+    "config_name",
+    "compiler",
+    "architecture",
+    "valid",
+    "test.hardware",
+    "test.path",
+    "boot.path",
+    "build.issue",
+    "boot.issue",
+    "test.issue",
+    "boot.platform",
+    "test.platform",
+]
+type FilterHandlers = dict[FilterFields, Callable]
+
+
 class InvalidComparisonOPError(
     Exception,
 ):
@@ -309,7 +332,7 @@ class FilterParams:
             "test": set(),
         }
 
-        self.filter_handlers = {
+        self.filter_handlers: FilterHandlers = {
             "boot.status": self._handle_boot_status,
             "boot.duration": self._handle_boot_duration,
             "test.status": self._handle_test_status,
