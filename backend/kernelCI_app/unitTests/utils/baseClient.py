d75883b98b5bda930c668794029e620894aa9835
Murilo Geraldini <murilo.geraldini@profusion.mobi>
2025-02-21 15:46:04 -0300

    feat(tests): add support for filters to clients

--- backend/kernelCI_app/unitTests/utils/baseClient.py
+++ backend/kernelCI_app/unitTests/utils/baseClient.py
@@ -1,17 +1,41 @@
 from abc import ABC
-from typing import Any
+from typing import Any, Optional
 from urllib.parse import urljoin, urlencode
+from kernelCI_app.helpers.filters import FilterFields
 
 
 class BaseClient(ABC):
     base_url = "http://localhost:8000/"
 
-    def get_endpoint(self, *, path: str, query: dict[str, Any] | None = None) -> str:
+    def get_endpoint(
+        self,
+        *,
+        path: str,
+        query: Optional[dict[str, Any]] = None,
+        filters: Optional[dict[FilterFields, Any]] = None,
+    ) -> str:
         url = urljoin(self.base_url, path)
+
+        query_string = ""
         if query is not None:
+            if filters:
+                query = self.join_query(query=query, filters=filters)
+
             # When a value has None value, the urlencode function change it into a string.
             # This logic remove None values from the query dict
             query = {k: v for k, v in query.items() if v is not None}
-            query_encoded = urlencode(query)
-            url = f"{url}?{query_encoded}"
+            query_string = f"?{urlencode(query)}"
+        elif filters is not None:
+            query_string = f"?{urlencode(self.get_filters(filters=filters))}"
+        url = f"{url}{query_string}"
         return url
+
+    def get_filters(self, *, filters: dict[FilterFields, Any]) -> dict[str, Any]:
+        prefix = "filter_"
+        return {f"{prefix}{key}": value for key, value in filters.items()}
+
+    def join_query(
+        self, *, query: dict[str, Any], filters: dict[FilterFields, Any]
+    ) -> dict[str, Any]:
+        filters = self.get_filters(filters=filters)
+        return dict(query, **filters)
