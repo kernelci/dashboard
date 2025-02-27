01798fc815562d9f1c23ca1956f7d290a7a9ac40
Marcelo Robert Santos <marcelo.santos@profusion.mobi>
2025-02-27 09:29:41 -0300

    feat: add test status history to test details endpoint
    Closes #899

--- backend/kernelCI_app/unitTests/utils/fields/tests.py
+++ backend/kernelCI_app/unitTests/utils/fields/tests.py
@@ -1,3 +1,14 @@
+issue_tests_expected_fields = [
+    "id",
+    "status",
+    "path",
+    "start_time",
+    "environment_compatible",
+    "environment_misc",
+    "tree_name",
+    "git_repository_branch",
+]
+
 test_expected_fields = [
     "id",
     "status",
@@ -7,4 +18,6 @@ test_expected_fields = [
     "environment_misc",
     "tree_name",
     "git_repository_branch",
+    "status_history",
+    "regression_type",
 ]
--- backend/kernelCI_app/views/testDetailsView.py
+++ backend/kernelCI_app/views/testDetailsView.py
@@ -1,7 +1,11 @@
 from http import HTTPStatus
 from kernelCI_app.helpers.errorHandling import create_api_error_response
-from kernelCI_app.models import Tests
-from kernelCI_app.typeModels.testDetails import TestDetailsResponse
+from kernelCI_app.queries.test import get_test_details_data, get_test_status_history
+from kernelCI_app.typeModels.databases import FAIL_STATUS, PASS_STATUS
+from kernelCI_app.typeModels.testDetails import (
+    PossibleRegressionType,
+    TestDetailsResponse,
+)
 from drf_spectacular.utils import extend_schema
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -9,41 +13,74 @@ from pydantic import ValidationError
 
 
 class TestDetails(APIView):
+    # TODO: create unit tests for this method
+    def process_test_status_history(
+        self, *, status_history: list[dict]
+    ) -> PossibleRegressionType:
+        history_task: PossibleRegressionType
+        first_test_flag = True
+        status_changed = False
+
+        for test in status_history:
+            test_status = test["status"]
+            if first_test_flag:
+                if test_status == PASS_STATUS:
+                    history_task = "pass"
+                    starting_status = PASS_STATUS
+                    opposite_status = FAIL_STATUS
+                elif test_status == FAIL_STATUS:
+                    history_task = "fail"
+                    starting_status = FAIL_STATUS
+                    opposite_status = PASS_STATUS
+                else:
+                    return "unstable"
+                first_test_flag = False
+                continue
+
+            is_inconclusive = test_status != PASS_STATUS and test_status != FAIL_STATUS
+
+            if test_status == opposite_status:
+                status_changed = True
+                if history_task == "pass":
+                    history_task = "fixed"
+                elif history_task == "fail":
+                    history_task = "regression"
+            if (status_changed and test_status == starting_status) or is_inconclusive:
+                return "unstable"
+
+        return history_task
+
     @extend_schema(
         responses=TestDetailsResponse,
     )
     def get(self, _request, test_id: str) -> Response:
-        fields = [
-            "id",
-            "build_id",
-            "status",
-            "path",
-            "log_excerpt",
-            "log_url",
-            "misc",
-            "environment_misc",
-            "start_time",
-            "environment_compatible",
-            "output_files",
-            "build__compiler",
-            "build__architecture",
-            "build__config_name",
-            "build__checkout__git_commit_hash",
-            "build__checkout__git_repository_branch",
-            "build__checkout__git_repository_url",
-            "build__checkout__git_commit_tags",
-            "build__checkout__tree_name",
-        ]
 
-        response = Tests.objects.filter(id=test_id).values(*fields).first()
+        response = get_test_details_data(test_id=test_id)
 
         if response is None:
             return create_api_error_response(
                 error_message="Test not found", status_code=HTTPStatus.OK
             )
 
+        status_history_response = get_test_status_history(
+            path=response["path"],
+            origin=response["build__checkout__origin"],
+            git_repository_url=response["build__checkout__git_repository_url"],
+            git_repository_branch=response["build__checkout__git_repository_branch"],
+            platform=response["environment_misc"]["platform"],
+            current_test_timestamp=response["field_timestamp"],
+        )
+
+        regression_type = self.process_test_status_history(
+            status_history=status_history_response
+        )
+
         try:
-            valid_response = TestDetailsResponse(**response)
+            valid_response = TestDetailsResponse(
+                **response,
+                status_history=status_history_response,
+                regression_type=regression_type,
+            )
         except ValidationError as e:
             return Response(data=e.json(), status=HTTPStatus.INTERNAL_SERVER_ERROR)
 
