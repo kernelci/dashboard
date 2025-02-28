af327d10df9ff4621063a7abe793a64f8587cb44
Marcelo Robert Santos <marcelo.santos@profusion.mobi>
2025-02-28 10:05:41 -0300

    refactor: reuse tooltip with icon component

--- dashboard/src/components/Issue/IssueTooltip.tsx
+++ dashboard/src/components/Issue/IssueTooltip.tsx
@@ -1,12 +1,7 @@
-import { LiaQuestionCircle } from 'react-icons/lia';
-
-import { FormattedMessage } from 'react-intl';
-
 import type { JSX } from 'react';
 
-import { Tooltip, TooltipContent, TooltipTrigger } from '@/components/Tooltip';
-import { formattedBreakLineValue } from '@/locales/messages';
 import { cn } from '@/lib/utils';
+import { TooltipIcon } from '@/components/Icons/TooltipIcon';
 
 export const IssueTooltip = ({
   iconClassName,
@@ -16,13 +11,10 @@ export const IssueTooltip = ({
   tooltipClassName?: string;
 }): JSX.Element => {
   return (
-    <Tooltip>
-      <TooltipTrigger>
-        <LiaQuestionCircle className={cn('h-5 w-5', iconClassName)} />
-      </TooltipTrigger>
-      <TooltipContent className={cn('font-normal', tooltipClassName)}>
-        <FormattedMessage id="issue.tooltip" values={formattedBreakLineValue} />
-      </TooltipContent>
-    </Tooltip>
+    <TooltipIcon
+      iconClassName={cn('h-5 w-5', iconClassName)}
+      contentClassName={cn('font-normal', tooltipClassName)}
+      tooltipId="issue.tooltip"
+    />
   );
 };
