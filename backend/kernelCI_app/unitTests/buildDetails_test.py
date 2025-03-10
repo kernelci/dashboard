ba9495167cb4344ab54be8ab29e3c6047808b867
Murilo Geraldini <murilo.geraldini@profusion.mobi>
2025-03-10 11:23:30 -0300

    test: build tests view
    Part of #972

--- backend/kernelCI_app/unitTests/buildDetails_test.py
+++ backend/kernelCI_app/unitTests/buildDetails_test.py
@@ -4,10 +4,16 @@ from kernelCI_app.unitTests.utils.asserts import (
     assert_status_code_and_error_response,
     assert_has_fields_in_response_content,
 )
-from kernelCI_app.unitTests.utils.fields.builds import build_expected_fields
+from kernelCI_app.unitTests.utils.fields.builds import (
+    build_details_expected_fields,
+    build_tests_expected_fields,
+)
 from kernelCI_app.utils import string_to_json
 import pytest
 from http import HTTPStatus
+
+
+client = BuildClient()
 
 
 @online
@@ -18,8 +24,9 @@ from http import HTTPStatus
         ("invalid_id", HTTPStatus.OK, True),
     ],
 )
-def test_get(build_id, status_code, has_error_body):
-    client = BuildClient()
+def test_get_build_details(
+    build_id: str, status_code: HTTPStatus, has_error_body: bool
+) -> None:
     response = client.get_build_details(build_id=build_id)
     content = string_to_json(response.content.decode())
     assert_status_code_and_error_response(
@@ -31,5 +38,38 @@ def test_get(build_id, status_code, has_error_body):
 
     if not has_error_body:
         assert_has_fields_in_response_content(
-            fields=build_expected_fields, response_content=content
+            fields=build_details_expected_fields, response_content=content
         )
+
+
+@online
+@pytest.mark.parametrize(
+    "build_id, status_code, has_error_body",
+    [
+        ("maestro:dummy_67cb759a180183719578307e_x86_64", HTTPStatus.OK, False),
+        ("maestro:67ce32e418018371957d36b1", HTTPStatus.OK, False),
+        ("invalid_id", HTTPStatus.OK, True),
+    ],
+)
+def test_get_build_tests(
+    pytestconfig, build_id: str, status_code: HTTPStatus, has_error_body: bool
+) -> None:
+    response = client.get_build_tests(build_id=build_id)
+    content = string_to_json(response.content.decode())
+    assert_status_code_and_error_response(
+        content=content,
+        response=response,
+        status_code=status_code,
+        should_error=has_error_body,
+    )
+
+    if not has_error_body:
+        assert_has_fields_in_response_content(
+            fields=build_tests_expected_fields, response_content=content[0]
+        )
+
+        if pytestconfig.getoption("--run-all") and len(content) > 1:
+            for test in content[1:]:
+                assert_has_fields_in_response_content(
+                    fields=build_tests_expected_fields, response_content=test
+                )
--- backend/kernelCI_app/unitTests/utils/client/buildClient.py
+++ backend/kernelCI_app/unitTests/utils/client/buildClient.py
@@ -10,6 +10,7 @@ class BuildClient(BaseClient):
         url = self.get_endpoint(path=path)
         return requests.get(url)
 
-    # TODO: Implement this method for testing BuildTestsView
-    def get_build_tests(self, *, build_id: str) -> requests.Response | Exception:
-        pass
+    def get_build_tests(self, *, build_id: str) -> requests.Response:
+        path = reverse("buildTests", kwargs={"build_id": build_id})
+        url = self.get_endpoint(path=path)
+        return requests.get(url)
