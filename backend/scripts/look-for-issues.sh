fcb15466cac1c25b5973fb3581dea76e7b12e7a0
Gustavo Padovan <gustavo.padovan@collabora.com>
2025-03-10 16:41:46 -0300

    notifications: add Tales to internal issues summary report

--- backend/scripts/look-for-issues.sh
+++ backend/scripts/look-for-issues.sh
@@ -2,7 +2,7 @@
 
 if [ -d "$DASHBOARD_BACKEND_DIR" ]; then
     cd $DASHBOARD_BACKEND_DIR
-    poetry run ./manage.py notifications --yes --send --to gus@collabora.com --action=new_issues
+    poetry run ./manage.py notifications --yes --send --to gus@collabora.com --cc tales.aparecida@redhat.com --action=new_issues
     echo "$0: finished execution at $(date)"
 else
     echo "$0: \$DASHBOARD_BACKEND_DIR not set."
