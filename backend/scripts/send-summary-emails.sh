1a432792beede6c8e4575a38f5f6313111958bff
Gustavo Padovan <gustavo.padovan@collabora.com>
2025-03-10 14:22:59 -0300

    notifications: cron script should have --yes for automated run

--- backend/scripts/send-summary-emails.sh
+++ backend/scripts/send-summary-emails.sh
@@ -3,7 +3,7 @@
 if [ -d "$DASHBOARD_BACKEND_DIR" ]; then
     cd $DASHBOARD_BACKEND_DIR
     poetry run ./manage.py \
-        notifications --send --add-mailing-lists --cc gus@collabora.com \
+        notifications --send --add-mailing-lists --cc gus@collabora.com --yes \
         --action=summary
     echo "$0: finished execution at $(date)"
 else
