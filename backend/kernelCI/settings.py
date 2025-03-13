b1916391ac393e4cba7a9dd403a8f8c395a92d43
Murilo Geraldini <murilo.geraldini@profusion.mobi>
2025-03-13 16:52:26 -0300

    feat: send notification when server errors happen
    Closes #1050

--- backend/kernelCI/settings.py
+++ backend/kernelCI/settings.py
@@ -92,6 +92,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "kernelCI_app.middleware.logServerErrorMiddleware.LogServerErrorMiddleware",
 ]
 
 ROOT_URLCONF = "kernelCI.urls"
