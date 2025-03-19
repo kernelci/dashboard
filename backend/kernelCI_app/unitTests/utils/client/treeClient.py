4687e6777f343823774656bd20e32a794986043e
francisco <francisco.silva@profusion.mobi>
2025-03-19 13:36:27 -0300

    test: add tree details test tests
    Close #984

--- backend/kernelCI_app/unitTests/utils/client/treeClient.py
+++ backend/kernelCI_app/unitTests/utils/client/treeClient.py
@@ -78,3 +78,14 @@ class TreeClient(BaseClient):
         path = reverse("treeDetailsBootsView", kwargs={"commit_hash": tree_id})
         url = self.get_endpoint(path=path, query=query.model_dump(), filters=filters)
         return requests.get(url)
+
+    def get_tree_details_tests(
+        self,
+        *,
+        tree_id: str,
+        query: TreeQueryParameters,
+        filters: dict[FilterFields, Any] | None = None,
+    ) -> requests.Response:
+        path = reverse("treeDetailsTestsView", kwargs={"commit_hash": tree_id})
+        url = self.get_endpoint(path=path, query=query.model_dump(), filters=filters)
+        return requests.get(url)
