f8b019d273cd77947aabb4c6f8ab06f9b2c98ff6
Murilo Geraldini <murilo.geraldini@profusion.mobi>
2025-04-02 14:49:25 -0300

    feat(api): add origins endpoint
    - Refactors set_query_cache to accept no params;
    - Adds origins endpoint.
    
    Part of #1149

---
+++ backend/requests/origins-get.sh
@@ -0,0 +1,25 @@
+http 'http://localhost:8000/api/origins/'
+
+# HTTP/1.1 200 OK
+# Allow: GET, HEAD, OPTIONS
+# Content-Length: 88
+# Content-Type: application/json
+# Cross-Origin-Opener-Policy: same-origin
+# Date: Wed, 02 Apr 2025 20:19:29 GMT
+# Referrer-Policy: same-origin
+# Server: WSGIServer/0.2 CPython/3.12.7
+# Vary: Accept, Cookie, origin
+# X-Content-Type-Options: nosniff
+# X-Frame-Options: DENY
+
+# [
+#     "0dayci",
+#     "arm",
+#     "broonie",
+#     "kernelci",
+#     "maestro",
+#     "microsoft",
+#     "redhat",
+#     "syzbot",
+#     "tuxsuite"
+# ]
