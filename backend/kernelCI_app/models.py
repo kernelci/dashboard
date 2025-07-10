42462a963bc0cc3b7771136331911a05ab12479b
Marcelo Robert Santos <marcelo.santos@profusion.mobi>
2025-07-10 11:11:56 -0300

    refactor: sync most models with kcidb
    Part of #1342

--- backend/kernelCI_app/models.py
+++ backend/kernelCI_app/models.py
@@ -2,12 +2,21 @@ from django.db import models
 from django.contrib.postgres.fields import ArrayField
 
 
+class StatusChoices(models.TextChoices):
+    PASS = "PASS"
+    FAIL = "FAIL"
+    SKIP = "SKIP"
+    ERROR = "ERROR"
+    MISS = "MISS"
+    DONE = "DONE"
+
+
 class Issues(models.Model):
     field_timestamp = models.DateTimeField(
         db_column="_timestamp", blank=True, null=True
     )
     id = models.TextField(primary_key=True)
-    version = models.IntegerField()
+    version = models.IntegerField()  # version is also part of the primary key in kcidb
     origin = models.TextField()
     report_url = models.TextField(blank=True, null=True)
     report_subject = models.TextField(blank=True, null=True)
@@ -35,7 +44,6 @@ class Checkouts(models.Model):
     git_commit_hash = models.TextField(blank=True, null=True)
     git_commit_name = models.TextField(blank=True, null=True)
     git_repository_branch = models.TextField(blank=True, null=True)
-    git_commit_tags = ArrayField(models.TextField(), blank=True, null=True)
     patchset_files = models.JSONField(blank=True, null=True)
     patchset_hash = models.TextField(blank=True, null=True)
     message_id = models.TextField(blank=True, null=True)
@@ -45,6 +53,9 @@ class Checkouts(models.Model):
     log_excerpt = models.CharField(max_length=16384, blank=True, null=True)
     valid = models.BooleanField(blank=True, null=True)
     misc = models.JSONField(blank=True, null=True)
+    git_commit_message = models.TextField(blank=True, null=True)
+    git_repository_branch_tip = models.BooleanField(blank=True, null=True)
+    git_commit_tags = ArrayField(models.TextField(), blank=True, null=True)
     origin_builds_finish_time = models.DateTimeField(blank=True, null=True)
     origin_tests_finish_time = models.DateTimeField(blank=True, null=True)
 
@@ -82,6 +93,10 @@ class Builds(models.Model):
 
 
 class Tests(models.Model):
+    class UnitPrefix(models.TextChoices):
+        METRIC = "metric"
+        BINARY = "binary"
+
     field_timestamp = models.DateTimeField(
         db_column="_timestamp", blank=True, null=True
     )
@@ -89,20 +104,25 @@ class Tests(models.Model):
     id = models.TextField(primary_key=True)
     origin = models.TextField()
     environment_comment = models.TextField(blank=True, null=True)
-    environment_compatible = ArrayField(
-        models.TextField(), blank=True, default=list, null=True
-    )
     environment_misc = models.JSONField(blank=True, null=True)
     path = models.TextField(blank=True, null=True)
     comment = models.TextField(blank=True, null=True)
     log_url = models.TextField(blank=True, null=True)
     log_excerpt = models.CharField(max_length=16384, blank=True, null=True)
-    status = models.TextField(blank=True, null=True)  # This field type is a guess.
+    status = models.CharField(
+        max_length=10, choices=StatusChoices.choices, blank=True, null=True
+    )
     start_time = models.DateTimeField(blank=True, null=True)
     duration = models.FloatField(blank=True, null=True)
     output_files = models.JSONField(blank=True, null=True)
-    input_files = models.JSONField(blank=True, null=True)
     misc = models.JSONField(blank=True, null=True)
+    number_value = models.FloatField(blank=True, null=True)
+    environment_compatible = ArrayField(models.TextField(), blank=True, null=True)
+    number_prefix = models.CharField(
+        max_length=10, choices=UnitPrefix.choices, blank=True, null=True
+    )
+    number_unit = models.TextField(blank=True, null=True)
+    input_files = models.JSONField(blank=True, null=True)
 
     class Meta:
         managed = False
@@ -115,10 +135,14 @@ class Incidents(models.Model):
     )
     id = models.TextField(primary_key=True)
     origin = models.TextField()
-    issue = models.ForeignKey(Issues, on_delete=models.DO_NOTHING)
+    issue_id = models.ForeignKey(Issues, on_delete=models.DO_NOTHING)
     issue_version = models.IntegerField()
-    build = models.ForeignKey(Builds, on_delete=models.DO_NOTHING)
-    test = models.ForeignKey(Tests, on_delete=models.DO_NOTHING)
+    build_id = models.ForeignKey(
+        Builds, on_delete=models.DO_NOTHING, null=True, blank=True
+    )
+    test_id = models.ForeignKey(
+        Tests, on_delete=models.DO_NOTHING, null=True, blank=True
+    )
     present = models.BooleanField(blank=True, null=True)
     comment = models.TextField(blank=True, null=True)
     misc = models.JSONField(blank=True, null=True)
