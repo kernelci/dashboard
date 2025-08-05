109c722430e04627477edb8d00b17998222365d0
Marcelo Robert Santos <marcelo.santos@profusion.mobi>
2025-08-05 11:10:13 -0300

    refactor: redesign buildDetails page
    Part of #1347

--- dashboard/src/components/BuildDetails/BuildDetails.tsx
+++ dashboard/src/components/BuildDetails/BuildDetails.tsx
@@ -18,7 +18,7 @@ import { formatDate, getTitle } from '@/utils/utils';
 
 import IssueSection from '@/components/Issue/IssueSection';
 
-import { shouldTruncate, truncateBigText, valueOrEmpty } from '@/lib/string';
+import { shouldTruncate, valueOrEmpty } from '@/lib/string';
 
 import { Sheet } from '@/components/Sheet';
 
@@ -47,11 +47,17 @@ import PageWithTitle from '@/components/PageWithTitle';
 import { MemoizedBuildDetailsOGTags } from '@/components/OpenGraphTags/BuildDetailsOGTags';
 import ButtonOpenLogSheet from '@/components/Button/ButtonOpenLogSheet';
 
-import MemoizedLinkItem from '@/components/DetailsLink';
-import { LinkIcon } from '@/components/Icons/Link';
 import { processLogData } from '@/hooks/useLogData';
 
 import { MemoizedKcidevFooter } from '@/components/Footer/KcidevFooter';
+
+import { TreeDetailsLink } from '@/components/TreeDetailsLink/TreeDetailsLink';
+
+import { DetailsInfoCard } from '@/components/Cards/DetailsInfoCard';
+
+import { LinkIcon } from '@/components/Icons/Link';
+
+import CopyButton from '@/components/Button/CopyButton';
 
 import BuildDetailsTestSection from './BuildDetailsTestSection';
 
@@ -94,30 +100,6 @@ const BuildDetails = ({
     [navigate],
   );
 
-  const treeDetailsLink = useMemo(
-    () => (
-      <MemoizedLinkItem
-        to="/tree/$treeId"
-        params={{ treeId: data?.git_commit_hash }}
-        state={s => s}
-        search={s => ({
-          origin: s.origin,
-          treeInfo: {
-            gitBranch: data?.git_repository_branch,
-            gitUrl: data?.git_repository_url,
-            treeName: data?.tree_name,
-            commitName: data?.git_commit_name,
-            headCommitHash: data?.git_commit_hash,
-          },
-        })}
-      >
-        {truncateBigText(data?.git_commit_hash)}
-        <LinkIcon className="text-blue text-xl" />
-      </MemoizedLinkItem>
-    ),
-    [data],
-  );
-
   const miscSection: ISection | undefined = useMemo(():
     | ISection
     | undefined => {
@@ -155,123 +137,165 @@ const BuildDetails = ({
     if (!data) {
       return [];
     }
+
+    const gitUrl = valueOrEmpty(data.git_repository_url);
+    const logUrl = valueOrEmpty(data.log_url);
+    const configUrl = valueOrEmpty(data.config_url);
+
     return [
       {
         title: buildDetailsTitle,
-        subtitle: <ButtonOpenLogSheet setSheetToLog={setSheetToLog} />,
-        leftIcon: <StatusIcon status={data?.status} />,
-        eyebrow: formatMessage({ id: 'buildDetails.buildDetails' }),
+        subtitle: (
+          <div>
+            <span className="text-[18px]">
+              {formatDate(valueOrEmpty(data.start_time), false, true)}
+            </span>
+            <ButtonOpenLogSheet setSheetToLog={setSheetToLog} />
+          </div>
+        ),
+        leftIcon: <StatusIcon status={data.status} showTooltip />,
         subsections: [
           {
             infos: [
               {
-                title: 'global.status',
-                linkText: data.status.toUpperCase(),
-                icon: <StatusIcon status={data.status} className="text-xl" />,
-              },
-              {
-                title: 'buildDetails.gitDescribe',
-                linkText: valueOrEmpty(data.git_commit_name),
-              },
-              {
-                title: 'global.tree',
-                linkText: valueOrEmpty(data.tree_name),
-              },
-              {
-                title: 'commonDetails.gitRepositoryBranch',
-                linkText: valueOrEmpty(data.git_repository_branch),
-              },
-              {
-                title: 'commonDetails.gitCommitHash',
-                linkText: valueOrEmpty(data.git_commit_hash),
-                linkComponent: treeDetailsLink,
-                copyValue: valueOrEmpty(data.git_commit_hash),
-              },
-              {
-                title: 'commonDetails.gitRepositoryUrl',
-                linkText: shouldTruncate(
-                  valueOrEmpty(data.git_repository_url),
-                ) ? (
-                  <TruncatedValueTooltip
-                    value={data.git_repository_url}
-                    isUrl={true}
-                  />
-                ) : (
-                  <span>{valueOrEmpty(data.git_repository_url)}</span>
+                children: (
+                  <div className="grid grid-cols-2 gap-4">
+                    <DetailsInfoCard
+                      cardTitle="buildDetails.buildInfo"
+                      data={[
+                        {
+                          title: 'global.status',
+                          linkText: data.status,
+                          icon: <StatusIcon status={data.status} />,
+                        },
+                        {
+                          title: 'global.architecture',
+                          linkText: valueOrEmpty(data.architecture),
+                        },
+                        {
+                          title: 'global.compiler',
+                          linkText: valueOrEmpty(data.compiler),
+                        },
+                        {
+                          title: 'global.config',
+                          linkText: valueOrEmpty(data.config_name),
+                        },
+                        {
+                          title: 'global.command',
+                          linkText: valueOrEmpty(data.command),
+                        },
+                        {
+                          title: 'global.buildTime',
+                          linkText: data.duration
+                            ? `${data.duration} sec`
+                            : '-',
+                        },
+                        {
+                          title: 'global.logs',
+                          linkText: shouldTruncate(logUrl) ? (
+                            <TruncatedValueTooltip value={logUrl} isUrl />
+                          ) : (
+                            logUrl
+                          ),
+                          link: data.log_url,
+                          icon: data.log_url ? (
+                            <LinkIcon className="text-blue text-xl" />
+                          ) : undefined,
+                        },
+                        {
+                          title: 'buildDetails.kernelConfig',
+                          linkText: shouldTruncate(configUrl) ? (
+                            <TruncatedValueTooltip value={configUrl} isUrl />
+                          ) : (
+                            configUrl
+                          ),
+                          link: data.config_url,
+                          icon: data.config_url ? (
+                            <LinkIcon className="text-blue text-xl" />
+                          ) : undefined,
+                        },
+                        {
+                          title: 'buildDetails.buildId',
+                          linkText: buildId,
+                        },
+                      ]}
+                    />
+                    <DetailsInfoCard
+                      cardTitle="commonDetails.gitInfo"
+                      data={[
+                        {
+                          title: 'global.tree',
+                          linkText: valueOrEmpty(data.tree_name),
+                        },
+                        {
+                          title: 'globalTable.branch',
+                          linkText: valueOrEmpty(data.git_repository_branch),
+                        },
+                        {
+                          title: 'global.commitHash',
+                          linkComponent: (
+                            <span className="flex">
+                              <TreeDetailsLink
+                                treeName={data.tree_name}
+                                gitBranch={data.git_repository_branch}
+                                commitHash={data.git_commit_hash}
+                                gitUrl={data.git_repository_url}
+                                commitName={data.git_commit_name}
+                              />
+                              {data.git_commit_hash && (
+                                <CopyButton value={data.git_commit_hash} />
+                              )}
+                            </span>
+                          ),
+                        },
+                        {
+                          title: 'global.repository',
+                          linkText: shouldTruncate(gitUrl) ? (
+                            <TruncatedValueTooltip value={gitUrl} isUrl />
+                          ) : (
+                            gitUrl
+                          ),
+                          link: data.git_repository_url,
+                          icon: data.git_repository_url ? (
+                            <LinkIcon className="text-blue text-xl" />
+                          ) : undefined,
+                        },
+                        {
+                          title: 'global.commitTags',
+                          linkText: valueOrEmpty(data.git_commit_tags?.[0]),
+                        },
+                        {
+                          title: 'buildDetails.gitDescribe',
+                          linkText: valueOrEmpty(data.git_commit_name),
+                        },
+                      ]}
+                    />
+                  </div>
                 ),
-                link: data.git_repository_url,
-              },
-              {
-                title: 'commonDetails.gitCommitTag',
-                linkText: valueOrEmpty(data.git_commit_tags?.[0]),
-              },
-              {
-                title: 'global.command',
-                linkText: valueOrEmpty(data.command),
-              },
-              {
-                title: 'global.architecture',
-                linkText: valueOrEmpty(data.architecture),
-              },
-              {
-                title: 'global.compiler',
-                linkText: valueOrEmpty(data.compiler),
-              },
-              {
-                title: 'global.config',
-                linkText: valueOrEmpty(data.config_name),
-              },
-              {
-                title: 'global.date',
-                linkText: formatDate(valueOrEmpty(data.start_time)),
-              },
-              {
-                title: 'global.buildTime',
-                linkText: data.duration ? `${data.duration} sec` : '-',
-              },
-              {
-                title: 'buildDetails.buildId',
-                linkText: buildId,
-              },
-            ],
-          },
-          {
-            infos: [
-              {
-                title: 'buildDetails.buildLogs',
-                linkText: (
-                  <TruncatedValueTooltip value={data.log_url} isUrl={true} />
-                ),
-                link: data.log_url,
-              },
-              {
-                title: 'buildDetails.kernelConfig',
-                linkText: (
-                  <TruncatedValueTooltip value={data.config_url} isUrl={true} />
-                ),
-                link: data.config_url,
               },
             ],
           },
         ],
       },
     ];
-  }, [
-    data,
-    buildDetailsTitle,
-    setSheetToLog,
-    formatMessage,
-    buildId,
-    treeDetailsLink,
-  ]);
+  }, [data, buildDetailsTitle, setSheetToLog, buildId]);
 
   const buildDetailsTabTitle: string = useMemo(() => {
-    const buildTitle = `${data?.tree_name} ${data?.git_commit_name}`;
+    const buildTitle =
+      data?.tree_name || data?.git_commit_name
+        ? `${data?.tree_name} ${data?.git_commit_name}`
+        : buildId;
     return formatMessage(
       { id: 'title.buildDetails' },
       { buildName: getTitle(buildTitle, isLoading) },
     );
-  }, [data?.git_commit_name, data?.tree_name, formatMessage, isLoading]);
+  }, [
+    buildId,
+    data?.git_commit_name,
+    data?.tree_name,
+    formatMessage,
+    isLoading,
+  ]);
 
   const kcidevComponent = useMemo(
     () => (
@@ -308,7 +332,7 @@ const BuildDetails = ({
       >
         <ErrorBoundary FallbackComponent={UnexpectedError}>
           <Sheet open={logOpen} onOpenChange={logOpenChange}>
-            <div className="flex flex-col gap-4 pb-8">
+            <div className="flex flex-col gap-4 pb-10">
               {breadcrumb}
               <SectionGroup sections={generalSections} />
               <BuildDetailsTestSection
