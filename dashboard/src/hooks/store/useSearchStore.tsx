28dbd3c90719ae9e9e1167aa41c5399f42ded836
Marcelo Robert Santos <marcelo.santos@profusion.mobi>
2025-08-26 17:07:14 -0300

    fix: return void on useSearchStore

--- dashboard/src/hooks/store/useSearchStore.tsx
+++ dashboard/src/hooks/store/useSearchStore.tsx
@@ -12,8 +12,9 @@ export const useSearchStore = create<SearchStore>()(
   persist(
     set => ({
       previousSearch: undefined,
-      updatePreviousSearch: (previousSearch): void =>
-        set(() => ({ previousSearch: previousSearch })),
+      updatePreviousSearch: (previousSearch): void => {
+        set(() => ({ previousSearch: previousSearch }));
+      },
     }),
     {
       name: 'previousSearchParams',
