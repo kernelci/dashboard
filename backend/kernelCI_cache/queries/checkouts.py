dc2e06b251fc4c09dca2c3b7e36997d48be68399
Marcelo Robert Santos <marcelo.santos@profusion.mobi>
2025-09-05 16:41:47 -0300

    feat: add better handling for cached stable trees
    There can be cases were a checkout is stable but is not cached, this condition is now checked. Cached checkouts that are unstable were also not being updated.
    
    Closes #1457

--- backend/kernelCI_cache/queries/checkouts.py
+++ backend/kernelCI_cache/queries/checkouts.py
@@ -1,6 +1,33 @@
-from kernelCI_cache.models import CheckoutsCache
+from django.db import connections
+from kernelCI_app.helpers.database import dict_fetchall
 
 
-def get_all_checkout_ids():
-    checkouts = CheckoutsCache.objects.using("cache").values("checkout_id").all()
-    return checkouts
+def get_cached_tree_listing_fast():
+    """Returns the most recent checkout of the trees that are cached in the sqlite database"""
+
+    query = """
+        SELECT * FROM (
+            SELECT
+                checkout_id,
+                tree_name,
+                git_repository_branch,
+                git_repository_url,
+                start_time,
+                unstable,
+                ROW_NUMBER() OVER (
+                    PARTITION BY
+                        tree_name,
+                        git_repository_branch,
+                        git_repository_url
+                    ORDER BY start_time DESC
+                ) as rn
+            FROM
+                checkouts_cache
+        ) WHERE rn = 1
+    """
+
+    with connections["cache"].cursor() as cursor:
+        cursor.execute(query)
+        records = dict_fetchall(cursor)
+
+    return records
