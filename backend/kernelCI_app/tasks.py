644cb86ccdc000e2f7fe3a0db454c133fafda143
Marcelo Robert Santos <marcelo.santos@profusion.mobi>
2025-09-05 16:41:47 -0300

    feat: add better handling for cached stable trees
    There can be cases were a checkout is stable but is not cached, this condition is now checked. Cached checkouts that are unstable were also not being updated.
    
    Closes #1457

--- backend/kernelCI_app/tasks.py
+++ backend/kernelCI_app/tasks.py
@@ -5,11 +5,14 @@ from kernelCI_app.queries.tree import (
 )
 from kernelCI_cache.checkouts import populate_checkouts_cache_db
 from kernelCI_cache.constants import NO_CACHE_ORIGINS, UNSTABLE_CHECKOUT_THRESHOLD
-from kernelCI_cache.queries.checkouts import get_all_checkout_ids
+from kernelCI_cache.queries.checkouts import get_cached_tree_listing_fast
 from datetime import timedelta
-from django.utils.timezone import now
+from django.utils.timezone import now, make_aware
 
 UPDATE_INTERVAL_IN_DAYS = 90
+
+type TreeIdentifier = tuple[str | None, str | None, str | None]
+"""A tuple to identify a unique tree: (tree_name, git_repository_branch, git_repository_url)"""
 
 
 def _is_checkout_done(*, checkout: dict | Checkouts) -> bool:
@@ -26,6 +29,14 @@ def _is_checkout_done(*, checkout: dict | Checkouts) -> bool:
 
 
 def _is_checkout_unstable(*, checkout: dict | Checkouts) -> bool:
+    """
+    Defines if a checkout is considered unstable or not.
+
+    It will be *stable* if both origin_builds_finish_time and origin_tests_finish_time are set
+      or if it is older than UNSTABLE_CHECKOUT_THRESHOLD.
+
+    Returns True if unstable, False if stable.
+    """
     unstable_threshold = now() - timedelta(days=UNSTABLE_CHECKOUT_THRESHOLD)
     if isinstance(checkout, Checkouts):
         start_time = checkout.start_time
@@ -40,33 +51,73 @@ def _is_checkout_unstable(*, checkout: dict | Checkouts) -> bool:
         return True
 
 
-def update_checkout_cache():
-    checkout_ids_for_update: set[str] = set()
+def get_checkout_ids_for_update(
+    *,
+    kcidb_checkouts: list[Checkouts],
+    sqlite_tree_keys: set[TreeIdentifier],
+    sqlite_trees_map: dict[TreeIdentifier, dict],
+) -> set[str]:
+    checkout_ids_for_update = set()
 
-    all_kcidb_checkouts = get_tree_listing_fast(
-        interval={"days": UPDATE_INTERVAL_IN_DAYS}
-    )
-
-    # TODO: instead of simply getting all ids, get the unstable tree data for comparison
-    all_sqlite_checkout_ids_records = get_all_checkout_ids()
-    all_sqlite_checkout_ids = [
-        record["checkout_id"] for record in all_sqlite_checkout_ids_records
-    ]
-
-    for checkout in all_kcidb_checkouts:
-        if not isinstance(checkout, Checkouts):
-            continue
-
+    for checkout in kcidb_checkouts:
         if checkout.origin in NO_CACHE_ORIGINS:
             continue
 
-        if checkout.id not in all_sqlite_checkout_ids:
+        checkout_key: TreeIdentifier = (
+            checkout.tree_name,
+            checkout.git_repository_branch,
+            checkout.git_repository_url,
+        )
+
+        # If the equivalent tree in sqlite is unstable, update it nonetheless
+        same_tree_on_sqlite = sqlite_trees_map.get(checkout_key)
+        if same_tree_on_sqlite["unstable"]:
+            checkout_ids_for_update.add(same_tree_on_sqlite["checkout_id"])
+
+        # Trees that aren't in the sqlite should be added
+        if checkout_key not in sqlite_tree_keys:
             checkout_ids_for_update.add(checkout.id)
             continue
 
-        is_unstable = _is_checkout_unstable(checkout=checkout)
-        if is_unstable:
+        # If the current checkout is unstable, update it
+        if _is_checkout_unstable(checkout=checkout):
             checkout_ids_for_update.add(checkout.id)
+            continue
+
+        # Even if the current checkout is stable, if it is newer than the cached one, update it
+        cached_start_time = same_tree_on_sqlite["start_time"]
+        if cached_start_time is not None:
+            if cached_start_time.tzinfo is None:
+                cached_start_time = make_aware(cached_start_time)
+            if checkout.start_time > cached_start_time:
+                checkout_ids_for_update.add(checkout.id)
+                continue
+
+    return checkout_ids_for_update
+
+
+def update_checkout_cache():
+    checkout_ids_for_update: set[str] = set()
+
+    kcidb_checkouts = get_tree_listing_fast(interval={"days": UPDATE_INTERVAL_IN_DAYS})
+
+    sqlite_checkouts = get_cached_tree_listing_fast()
+    sqlite_trees_map: dict[TreeIdentifier, dict] = {}
+    sqlite_tree_keys: set[TreeIdentifier] = set()
+    for cached_checkout in sqlite_checkouts:
+        tree_ident: TreeIdentifier = (
+            cached_checkout["tree_name"],
+            cached_checkout["git_repository_branch"],
+            cached_checkout["git_repository_url"],
+        )
+        sqlite_tree_keys.add(tree_ident)
+        sqlite_trees_map[tree_ident] = cached_checkout
+
+    checkout_ids_for_update = get_checkout_ids_for_update(
+        kcidb_checkouts=kcidb_checkouts,
+        sqlite_tree_keys=sqlite_tree_keys,
+        sqlite_trees_map=sqlite_trees_map,
+    )
 
     updated_checkouts_data = get_tree_listing_data_by_checkout_id(
         checkout_ids=list(checkout_ids_for_update),
