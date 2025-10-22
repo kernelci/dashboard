8b3d868f9bd2d69e3abf1c39eec163ffedeba716
Marcelo Robert Santos <marcelo.santos@profusion.mobi>
2025-10-22 09:21:37 -0300

    fix(tests): correct hardwareListing test

--- k6/tests/hardwareListing.js
+++ k6/tests/hardwareListing.js
@@ -1,8 +1,21 @@
 import http from "k6/http";
 import { check } from "k6";
 
+export const options = {
+    iterations: 100,
+};
+
 export default function () {
-    const defaultRes = http.get('http://test-backend:8000/api/hardware');
+    // endTimestamp is "now" or the closest date
+    const endTimestampInSeconds = Math.floor(new Date('2025-10-21T19:30:00Z').getTime() / 1000);
+    // startTimestamp is x days before endTimestamp
+    const startTimestampInSeconds = Math.floor(new Date('2025-10-16T19:30:00Z').getTime() / 1000);
+
+    const defaultRes = http.get("http://test-backend:8000/api/hardware?" +
+        `startTimestampInSeconds=${startTimestampInSeconds}&` +
+        `endTimestampInSeconds=${endTimestampInSeconds}&` +
+        "origin=maestro");
+
     check(defaultRes, {
         'status is 200': (r) => r.status === 200,
         'response time < 500ms': (r) => r.timings.duration < 500,
