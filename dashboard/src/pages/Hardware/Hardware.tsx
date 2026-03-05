88257c04531cdbd3142856f5d736e55d53cf0827
Marcelo Robert Santos <marcelo.santos@profusion.mobi>
2026-03-05 16:33:11 -0300

    feat: add responsivity to listing search
    - Moves control of the searchbox from each page to the topbar;
    - Improves custom dialog to be more custom;
    - Improves searchbox with responsivity showing a small button and a dialog on small screens
    
    Closes #1416

--- dashboard/src/pages/Hardware/Hardware.tsx
+++ dashboard/src/pages/Hardware/Hardware.tsx
@@ -1,13 +1,9 @@
-import type { ChangeEvent, JSX } from 'react';
-import { useCallback } from 'react';
+import type { JSX } from 'react';
 
-import { useIntl } from 'react-intl';
-
-import { useNavigate, useSearch } from '@tanstack/react-router';
+import { useSearch } from '@tanstack/react-router';
 
 import HardwareListingPage from '@/pages/Hardware/HardwareListingPage';
 
-import DebounceInput from '@/components/DebounceInput/DebounceInput';
 import { MemoizedListingOGTags } from '@/components/OpenGraphTags/ListingOGTags';
 import { OldPageBanner } from '@/components/Banner/PageBanner';
 import { useFeatureFlag } from '@/hooks/useFeatureFlag';
@@ -23,38 +19,9 @@ const Hardware = ({
     from: urlFromMap.search,
   });
 
-  const navigate = useNavigate({ from: urlFromMap.navigate });
-
-  const onInputSearchTextChange = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      navigate({
-        search: previousSearch => ({
-          ...previousSearch,
-          hardwareSearch: e.target.value,
-        }),
-      });
-    },
-    [navigate],
-  );
-
-  const { formatMessage } = useIntl();
-
   return (
     <>
       <MemoizedListingOGTags monitor="/hardware" search={hardwareSearch} />
-      <div className="fixed top-0 z-10 mx-[380px] flex w-full pt-5 pr-12 pl-6">
-        <div className="flex w-2/3 items-center px-6">
-          <DebounceInput
-            debouncedSideEffect={onInputSearchTextChange}
-            className="w-2/3"
-            type="text"
-            startingValue={hardwareSearch}
-            placeholder={formatMessage({
-              id: 'hardware.searchPlaceholder',
-            })}
-          />
-        </div>
-      </div>
       {hardwareListingVersion !== 'v1' && (
         <OldPageBanner
           pageNameId="hardwareListing.bannerTitle"
--- dashboard/src/pages/TreeListing/TreesV2.tsx
+++ dashboard/src/pages/TreeListing/TreesV2.tsx
@@ -1,11 +1,6 @@
-import type { ChangeEvent, JSX } from 'react';
-import { useCallback } from 'react';
+import type { JSX } from 'react';
+import { useSearch } from '@tanstack/react-router';
 
-import { useIntl } from 'react-intl';
-
-import { useNavigate, useSearch } from '@tanstack/react-router';
-
-import DebounceInput from '@/components/DebounceInput/DebounceInput';
 import { MemoizedListingOGTags } from '@/components/OpenGraphTags/ListingOGTags';
 import TreeListingV2 from '@/components/TreeListingPage/TreeListingV2';
 import { NewPageBanner } from '@/components/Banner/PageBanner';
@@ -22,36 +17,9 @@ const TreeListingPageV2 = ({
     from: urlFromMap.search,
   });
 
-  const navigate = useNavigate({ from: urlFromMap.navigate });
-
-  const onInputSearchTextChange = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      navigate({
-        search: previousSearch => ({
-          ...previousSearch,
-          treeSearch: e.target.value,
-        }),
-      });
-    },
-    [navigate],
-  );
-
-  const { formatMessage } = useIntl();
-
   return (
     <>
       <MemoizedListingOGTags monitor="/tree" search={treeSearch} />
-      <div className="fixed top-0 z-10 mx-[380px] flex w-full pt-5 pr-12 pl-6">
-        <div className="flex w-2/3 items-center px-6">
-          <DebounceInput
-            debouncedSideEffect={onInputSearchTextChange}
-            className="w-2/3"
-            type="text"
-            startingValue={treeSearch}
-            placeholder={formatMessage({ id: 'tree.searchPlaceholder' })}
-          />
-        </div>
-      </div>
       {treeListingVersion !== 'v2' && (
         <NewPageBanner
           pageNameId="treeListing.treeListing"
