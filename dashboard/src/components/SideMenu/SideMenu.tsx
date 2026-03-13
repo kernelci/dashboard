78b38c55a3e12d1dbf716d99f9add3ac4a30ca3d
Alan Peixinho <alanpeixinho81@gmail.com>
2026-03-13 09:21:13 -0300

    fix: sidemenu without vertical scrolling (#1798)
    * Uses a sticky side menu fixed on top of the page
    
     Closes #1413

--- dashboard/src/components/SideMenu/SideMenu.tsx
+++ dashboard/src/components/SideMenu/SideMenu.tsx
@@ -4,7 +4,7 @@ import SideMenuContent from './SideMenuContent';
 
 const SideMenu = (): JSX.Element => {
   return (
-    <div className="bg-bg-secondary hidden md:block">
+    <div className="bg-bg-secondary sticky top-0 hidden h-screen overflow-y-scroll md:block">
       <SideMenuContent className="min-h-screen" />
     </div>
   );
