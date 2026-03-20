3d56c90c428a44cb71bde23834f90c474d212a52
Gustavo Flores <gustavo.flores@profusion.mobi>
2026-03-20 10:42:48 -0300

    test: add unit tests for process_pending_helpers functions

---
+++ backend/kernelCI_app/constants/process_pending.py
@@ -0,0 +1,9 @@
+ROLLUP_STATUS_FIELDS = {
+    "PASS": "pass_tests",
+    "FAIL": "fail_tests",
+    "SKIP": "skip_tests",
+    "ERROR": "error_tests",
+    "MISS": "miss_tests",
+    "DONE": "done_tests",
+    "NULL": "null_tests",
+}
