321f043bd3834f142bed000f98124b90b4495a71
Gustavo Flores <gustavo.flores@profusion.mobi>
2026-03-24 23:07:08 -0300

    feat: update feature flag versions to v2 in useFeatureFlag hook

--- dashboard/src/hooks/useFeatureFlag.ts
+++ dashboard/src/hooks/useFeatureFlag.ts
@@ -8,8 +8,8 @@ export const useFeatureFlag = (): FeatureFlags => {
   return {
     showDev: import.meta.env.VITE_FEATURE_FLAG_SHOW_DEV ?? false,
     treeListingVersion:
-      import.meta.env.VITE_FEATURE_FLAG_TREE_LISTING_VERSION ?? 'v1',
+      import.meta.env.VITE_FEATURE_FLAG_TREE_LISTING_VERSION ?? 'v2',
     hardwareListingVersion:
-      import.meta.env.VITE_FEATURE_FLAG_HARDWARE_LISTING_VERSION ?? 'v1',
+      import.meta.env.VITE_FEATURE_FLAG_HARDWARE_LISTING_VERSION ?? 'v2',
   };
 };
