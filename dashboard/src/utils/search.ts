gustavobtflores/kernelci-dashboard

feat: add hardware tree selection flow on the frontend

dashboard/src/utils/search.ts
@@ -151,6 +151,10 @@ const generalMinifiedParams: Record<SearchParamsKeys, string> = {
   endTimestampInSeconds: 'et',
   issueVersion: 'iv',
   logOpen: 'l',
+  treeName: 't',
+  gitRepositoryUrl: 'hgu',
+  gitBranch: 'gb',
+  gitCommitHash: 'hch',
 } as const;
 
 const treeInfoMinifiedParams: Record<keyof TTreeInformation, string> = {

kernelci/dashboard

Signed-off-by: Gustavo Flores <gustavo.flores@profusion.mobi>

dashboard/src/utils/search.ts
@@ -151,6 +151,10 @@ const generalMinifiedParams: Record<SearchParamsKeys, string> = {
   endTimestampInSeconds: 'et',
   issueVersion: 'iv',
   logOpen: 'l',
+  treeName: 't',
+  gitRepositoryUrl: 'gu',
+  gitBranch: 'gb',
+  gitCommitHash: 'ch',
 } as const;
 
 const treeInfoMinifiedParams: Record<keyof TTreeInformation, string> = {