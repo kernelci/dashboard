gustavobtflores/kernelci-dashboard

refactor: enhance view_cache function to accept custom timeout and update hardware-by-revision path cache duration

backend/kernelCI_app/urls.py
@@ -13,8 +13,8 @@ timeout = settings.CACHE_TIMEOUT
 cache = cache_page(timeout)
 
 
-def view_cache(view):
-    return cache(view.as_view())
+def view_cache(view, timeout: int = settings.CACHE_TIMEOUT):
+    return cache_page(timeout)(view.as_view())
 
 
 urlpatterns = [
@@ -153,7 +153,7 @@ urlpatterns = [
     path("hardware/", view_cache(views.HardwareView), name="hardware"),
     path(
         "hardware-by-revision/",
-        view_cache(views.HardwareByRevisionView),
+        view_cache(views.HardwareByRevisionView, timeout=60 * 60),
         name="hardwareByRevision",
     ),
     path("hardware-v2/", view_cache(views.HardwareViewV2), name="hardware-v2"),

kernelci/dashboard

Signed-off-by: Gustavo Flores <gustavo.flores@profusion.mobi>

backend/kernelCI_app/urls.py
@@ -9,12 +9,9 @@ from drf_spectacular.views import (
 
 from kernelCI_app import views
 
-timeout = settings.CACHE_TIMEOUT
-cache = cache_page(timeout)
 
-
-def view_cache(view):
-    return cache(view.as_view())
+def view_cache(view, timeout: int = settings.CACHE_TIMEOUT):
+    return cache_page(timeout)(view.as_view())
 
 
 urlpatterns = [
@@ -117,7 +114,7 @@ urlpatterns = [
     path("log-downloader/", view_cache(views.LogDownloaderView), name="logDownloader"),
     path(
         "hardware/selectors/",
-        view_cache(views.HardwareSelectorsView),
+        view_cache(views.HardwareSelectorsView, timeout=60 * 60),
         name="hardwareSelectors",
     ),
     path(